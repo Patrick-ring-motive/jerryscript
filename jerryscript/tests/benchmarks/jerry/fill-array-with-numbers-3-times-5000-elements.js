45f3e4dd17a4e0da987c40b6d6c5476b0e85afa2
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:51:57 -0500

    style: js-beautify formatting (jerryscript/tests/benchmarks/jerry/fill-array-with-numbers-3-times-5000-elements.js)

--- jerryscript/tests/benchmarks/jerry/fill-array-with-numbers-3-times-5000-elements.js
+++ jerryscript/tests/benchmarks/jerry/fill-array-with-numbers-3-times-5000-elements.js
@@ -12,12 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-for (i = 0; i < 3; i++)
-{
+for (i = 0; i < 3; i++) {
   a = [];
 
-  for (j = 0; j < 5000; j++)
-  {
+  for (j = 0; j < 5000; j++) {
     a[j] = j;
   }
 }
