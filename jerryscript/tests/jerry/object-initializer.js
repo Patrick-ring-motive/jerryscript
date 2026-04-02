0f6842af0a7640cfec0bb2f147cf84eb6fc7d356
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:10:01 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/object-initializer.js)

--- jerryscript/tests/jerry/object-initializer.js
+++ jerryscript/tests/jerry/object-initializer.js
@@ -13,30 +13,30 @@
 // limitations under the License.
 
 switch (1) {
-default:
-  var o = {
-    value: 10,
-    func() {
-      return 234 + this.value;
-    },
-    ["a" + "b"]() {
-      return 456 - this.value;
+  default:
+    var o = {
+      value: 10,
+      func() {
+        return 234 + this.value;
+      },
+      ["a" + "b"]() {
+        return 456 - this.value;
+      }
     }
-  }
 }
 
 assert(o.func() === 244);
 assert(o.ab() === 446);
 
 switch (1) {
-default:
-  var ab = 5;
-  var cd = 6;
-  o = {
-    ab,
-    cd: 8,
-    cd
-  }
+  default:
+    var ab = 5;
+    var cd = 6;
+    o = {
+      ab,
+      cd: 8,
+      cd
+    }
 }
 
 assert(o.ab === 5);
@@ -57,21 +57,37 @@ exception_expected('({ 13 })');
 exception_expected('({ "x" })');
 
 switch (1) {
-default:
-  // These forms are valid.
-  ({ true: true });
-  ({ 13: 13 });
-  ({ "x": "x" });
+  default:
+    // These forms are valid.
+    ({
+      true: true
+    });
+    ({
+      13: 13
+    });
+    ({
+      "x": "x"
+    });
 
-  var get = 8;
-  var set = 12;
-  var o = ({ get, set });
+    var get = 8;
+    var set = 12;
+    var o = ({
+      get,
+      set
+    });
 
-  assert(o.get == 8);
-  assert(o.set == 12);
+    assert(o.get == 8);
+    assert(o.set == 12);
 }
 
-var obj = { get() { return 5; }, set() { return 6; } };
+var obj = {
+  get() {
+    return 5;
+  },
+  set() {
+    return 6;
+  }
+};
 
-assert (obj.get() === 5);
-assert (obj.set() === 6);
+assert(obj.get() === 5);
+assert(obj.set() === 6);
