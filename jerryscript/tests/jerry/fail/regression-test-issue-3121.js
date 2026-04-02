1ac89b76a608b19692d2795d0d9a13bc9b15a102
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:00:58 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/regression-test-issue-3121.js)

--- jerryscript/tests/jerry/fail/regression-test-issue-3121.js
+++ jerryscript/tests/jerry/fail/regression-test-issue-3121.js
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var Base = function ( ) { }.bind( );
-Object.defineProperty ( Base , 'prototype' , { get : function ( ) {$} } );
-class C extends Base { }
+var Base = function() {}.bind();
+Object.defineProperty(Base, 'prototype', {
+  get: function() {
+    $
+  }
+});
+class C extends Base {}
