d98c1634eeba5bd9d99641b8791498872509c786
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:03:10 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/function-await3.js)

--- jerryscript/tests/jerry/function-await3.js
+++ jerryscript/tests/jerry/function-await3.js
@@ -14,13 +14,11 @@
 
 var successCount = 0
 
-async function f1()
-{
+async function f1() {
   throw 1
 }
 
-async function f2()
-{
+async function f2() {
   try {
     assert(await f1() && false)
   } catch (e) {
@@ -31,19 +29,21 @@ async function f2()
   }
 }
 
-async function f3()
-{
+async function f3() {
   return await f2() + 1
 }
 
-async function f4()
-{
+async function f4() {
   return await f1()
 }
 
-async function f5()
-{
-  var o = { a:f2, b:f2, c:f2, d:f2 }
+async function f5() {
+  var o = {
+    a: f2,
+    b: f2,
+    c: f2,
+    d: f2
+  }
 
   for (i in o) {
     var p1 = f3()
