0d237b2dd1813e55af98ccbb003c060236a58961
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:02:30 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/for-parse.js)

--- jerryscript/tests/jerry/for-parse.js
+++ jerryscript/tests/jerry/for-parse.js
@@ -21,9 +21,9 @@ function parse(txt) {
   }
 }
 
-parse("for (;;;) print('hello')") 
-parse("for (,,) print('hello')") 
-parse("for (i = 0; i < 10; i++; ) print('i: ' + i)") 
+parse("for (;;;) print('hello')")
+parse("for (,,) print('hello')")
+parse("for (i = 0; i < 10; i++; ) print('i: ' + i)")
 parse("for (i = 0; i < 10; i++)")
 parse("for i = 0; i < 10; i++ {}")
 parse("for (i = 0;; i < 10; i++) {}")
@@ -33,11 +33,11 @@ parse("for (i = 0, j = 5; i < 10 || j < 10; i++ j++) {}")
 parse("for (i = 0, j = 5; i < 10 j < 10; i++, j++) {}")
 
 var doubleFor =
-"for (i = 0; i < 5; i++) { "+ 
-"  for (j = 0; j < 5; j++) { print('i: ' + i + ' j: ' + j) }";
+  "for (i = 0; i < 5; i++) { " +
+  "  for (j = 0; j < 5; j++) { print('i: ' + i + ' j: ' + j) }";
 parse(doubleFor)
 
 var doubleFor2 =
-"for (i = 0; i < 5; i++) { "+ 
-"  for (j = 0; j < 5; j++) { print('i: ' + i + ' j: ' + j)";
+  "for (i = 0; i < 5; i++) { " +
+  "  for (j = 0; j < 5; j++) { print('i: ' + i + ' j: ' + j)";
 parse(doubleFor2)
