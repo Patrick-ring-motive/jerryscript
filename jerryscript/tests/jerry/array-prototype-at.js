e63c3b1bdc12dfa31da13f718a5a24ee00797df1
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:53:30 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-prototype-at.js)

--- jerryscript/tests/jerry/array-prototype-at.js
+++ jerryscript/tests/jerry/array-prototype-at.js
@@ -21,8 +21,8 @@ assert(index === 'Apple');
 assert(array[index] === undefined);
 
 assert(array.at(array.length) === undefined);
-assert(array.at(array.length+1) === undefined);
-assert(array.at(array.length-1) === 'Apple');
+assert(array.at(array.length + 1) === undefined);
+assert(array.at(array.length - 1) === 'Apple');
 assert(array.at("1") === 'Banana');
 assert(array.at(-1) === 'Apple');
 assert(array.at("-1") === 'Apple');
@@ -31,20 +31,24 @@ assert(array.at("-20") === undefined);
 /* 7 */
 var obj = {}
 obj.length = 1;
-Object.defineProperty(obj, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(obj, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 obj.at = Array.prototype.at;
 
 try {
   obj.at(0);
   assert(false);
-} catch(e) {
+} catch (e) {
   assert(e.message === "foo");
   assert(e instanceof ReferenceError);
 }
 
 try {
   Array.prototype.at.call(undefined)
-  assert (false);
-} catch(e) {
+  assert(false);
+} catch (e) {
   assert(e instanceof TypeError);
 }
