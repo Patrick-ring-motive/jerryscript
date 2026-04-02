a4b8194d1c20114222d57405266b85791d43994d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:08:44 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/new-target-for-string.js)

--- jerryscript/tests/jerry/new-target-for-string.js
+++ jerryscript/tests/jerry/new-target-for-string.js
@@ -13,21 +13,21 @@
  * limitations under the License.
  */
 
- try {
-  Reflect.construct (String, "");
-  assert (false);
+try {
+  Reflect.construct(String, "");
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
 
 try {
-  Reflect.construct (String, "randomText");
-  assert (false);
+  Reflect.construct(String, "randomText");
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
 
 class MyString extends String {};
-var s1= new MyString();
+var s1 = new MyString();
 
 assert(Object.getPrototypeOf(s1) == MyString.prototype)
