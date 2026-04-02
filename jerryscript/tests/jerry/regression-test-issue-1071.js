7cd515dd73aabc25976d168eba6c55bc1999d619
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:15:01 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-1071.js)

--- jerryscript/tests/jerry/regression-test-issue-1071.js
+++ jerryscript/tests/jerry/regression-test-issue-1071.js
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 var d = new Date(864858163349847396, 1, 1, 1, 1, 1, 1, 1)
-assert (d.toGMTString() === "Invalid Date");
+assert(d.toGMTString() === "Invalid Date");
 
 d = new Date(864858, 1, 1, 1, 1, 1, 1, 1)
-assert (d.toGMTString() === "Invalid Date");
+assert(d.toGMTString() === "Invalid Date");
 
 d = new Date(86485, 1, 1, 1, 1, 1, 1, 1)
-assert (d.toGMTString() !== "Invalid Date");
+assert(d.toGMTString() !== "Invalid Date");
