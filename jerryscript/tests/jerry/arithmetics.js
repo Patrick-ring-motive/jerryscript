d6c99172246e392531a84e8b878c1fee1e020b45
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:53:15 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/arithmetics.js)

--- jerryscript/tests/jerry/arithmetics.js
+++ jerryscript/tests/jerry/arithmetics.js
@@ -30,12 +30,13 @@ assert((number * 10) == 810);
 assert((number / 9) == 9);
 assert((number % 79) == 2);
 
-var num1 = 1234567, num2 = 1234000;
+var num1 = 1234567,
+  num2 = 1234000;
 assert((num1 % num2) == 567);
 
-assert (1 / (-1 % -1) < 0);
-assert (1 / (-1 % 1) < 0);
-assert (1 / (1 % -1) > 0);
-assert (1 / (1 % 1) > 0);
+assert(1 / (-1 % -1) < 0);
+assert(1 / (-1 % 1) < 0);
+assert(1 / (1 % -1) > 0);
+assert(1 / (1 % 1) > 0);
 
-assert (eval ("x\n\n=\n\n6\n\n/\n\n3") === 2)
+assert(eval("x\n\n=\n\n6\n\n/\n\n3") === 2)
