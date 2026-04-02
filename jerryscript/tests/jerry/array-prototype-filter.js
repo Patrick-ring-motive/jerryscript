b080672dfc12a818076ac23da7f0625cb4d8e5cb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:53:44 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-prototype-filter.js)

--- jerryscript/tests/jerry/array-prototype-filter.js
+++ jerryscript/tests/jerry/array-prototype-filter.js
@@ -28,7 +28,7 @@ for (i = 0; i < filtered.length; i++) {
 
 var array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-function g (arg1, arg2, arg3) {
+function g(arg1, arg2, arg3) {
   if (arg2 % 2 === 0) {
     return true;
   } else {
@@ -43,16 +43,22 @@ assert(filtered[1] === 3);
 assert(filtered[2] === 5);
 assert(filtered[3] === 7);
 
-var arr = [1,2];
+var arr = [1, 2];
 Array.prototype[0] = 3;
-var newArr = arr.filter(function() { return true; });
+var newArr = arr.filter(function() {
+  return true;
+});
 delete Array.prototype[0];
 assert(newArr.hasOwnProperty("0"));
 assert(newArr[0] === 1);
 
 // Checking behavior when unable to get length
 var obj = {};
-Object.defineProperty(obj, 'length', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(obj, 'length', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 obj.filter = Array.prototype.filter;
 
 try {
@@ -66,7 +72,11 @@ try {
 // Checking behavior when unable to get element
 var obj = {}
 obj.length = 1;
-Object.defineProperty(obj, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(obj, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 obj.filter = Array.prototype.filter
 
 try {
