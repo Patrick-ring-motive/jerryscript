e414129e3c98bded3599d2614c0b1f66b73532b4
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:17:58 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-2488.js)

--- jerryscript/tests/jerry/regression-test-issue-2488.js
+++ jerryscript/tests/jerry/regression-test-issue-2488.js
@@ -12,5 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-Object.defineProperty(Array.prototype, 0, {set: function() {throw "MyError"}});
+Object.defineProperty(Array.prototype, 0, {
+  set: function() {
+    throw "MyError"
+  }
+});
 Promise.all([0]);
