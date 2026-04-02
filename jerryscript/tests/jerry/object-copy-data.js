f6e016de8ca63f3121cfd4c709d9c8d27d990769
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:09:31 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/object-copy-data.js)

--- jerryscript/tests/jerry/object-copy-data.js
+++ jerryscript/tests/jerry/object-copy-data.js
@@ -12,13 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function checkOwnProperties(obj, propList)
-{
+function checkOwnProperties(obj, propList) {
   names = Object.getOwnPropertyNames(obj)
   assert(names.length === propList.length)
 
-  for (var i = 0; i < propList.length; ++i)
-  {
+  for (var i = 0; i < propList.length; ++i) {
     assert(names[i] === propList[i])
 
     var descriptor = Object.getOwnPropertyDescriptor(obj, names[i])
@@ -26,42 +24,82 @@ function checkOwnProperties(obj, propList)
   }
 }
 
-var o = {..."a" + "bc"}
+var o = {
+  ..."a" + "bc"
+}
 
 assert(o[0] === "a")
 assert(o[1] === "b")
 assert(o[2] === "c")
 checkOwnProperties(o, ["0", "1", "2"])
 
-s = { a:3.5, b() {}, get c() { return "Prop" } }
-o = {...null, ...undefined, ...s}
+s = {
+  a: 3.5,
+  b() {},
+  get c() {
+    return "Prop"
+  }
+}
+o = {
+  ...null,
+  ...undefined,
+  ...s
+}
 
 assert(o.a === 3.5)
 assert(o.b === s.b)
 assert(o.c === "Prop")
 checkOwnProperties(o, ["a", "b", "c"])
 
-s = { a:"X", b:-1.25, c:"Str" }
-o = { get a() {}, set b(v) {}, ...s, c() { return 1 }}
+s = {
+  a: "X",
+  b: -1.25,
+  c: "Str"
+}
+o = {
+  get a() {},
+  set b(v) {},
+  ...s,
+  c() {
+    return 1
+  }
+}
 
 assert(o.a === "X")
 assert(o.b === -1.25)
 assert(o.c() === 1)
 checkOwnProperties(o, ["a", "b", "c"])
 
-s = { p1:[1,2], "(a)":"Msg" }
-Object.defineProperty(s, 'nonEnumerable', { value: 8.75, writable: true, enumerable:false });
+s = {
+  p1: [1, 2],
+  "(a)": "Msg"
+}
+Object.defineProperty(s, 'nonEnumerable', {
+  value: 8.75,
+  writable: true,
+  enumerable: false
+});
 assert(s.nonEnumerable === 8.75)
 
-o = { ...s, "p!":5, ...s, ...s }
+o = {
+  ...s,
+  "p!": 5,
+  ...s,
+  ...s
+}
 assert(o.p1 === s.p1)
 assert(o["(a)"] === "Msg")
 assert(o["p!"] === 5)
 checkOwnProperties(o, ["p1", "(a)", "p!"])
 
 var sym = Symbol('Any')
-s = [ { [sym]:5, a:6 } ]
-o = { ...((s))[0] }
+s = [{
+  [sym]: 5,
+  a: 6
+}]
+o = {
+  ...((s))[0]
+}
 
 assert(o[sym] === 5)
 assert(o.a === 6)
