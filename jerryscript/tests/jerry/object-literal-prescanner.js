772edc187837c61a8946dfb51c09cf9d9789eb22
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:10:13 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/object-literal-prescanner.js)

--- jerryscript/tests/jerry/object-literal-prescanner.js
+++ jerryscript/tests/jerry/object-literal-prescanner.js
@@ -13,26 +13,23 @@
 // limitations under the License.
 
 switch (true) {
-default:
-  var obj = {
-    value: 0,
-    set
-      member
-      (x)
-    {
-      // Multiple statements.
-      this.value = x + 4;
-      this.value += 2;
-    },
-    get"member"() {
-      // Multiple statements.
-      this.value = this.value + 1;
-      return this.value;
-    },
-    get 3() {
-      return 3;
+  default:
+    var obj = {
+      value: 0,
+      set member(x) {
+        // Multiple statements.
+        this.value = x + 4;
+        this.value += 2;
+      },
+      get "member"() {
+        // Multiple statements.
+        this.value = this.value + 1;
+        return this.value;
+      },
+      get 3() {
+        return 3;
+      }
     }
-  }
 }
 
 obj["member"] = 10;
