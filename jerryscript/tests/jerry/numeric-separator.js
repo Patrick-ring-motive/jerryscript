5aea9475cfeda05ba939e6d42524cbc52de1bcfe
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:09:21 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/numeric-separator.js)

--- jerryscript/tests/jerry/numeric-separator.js
+++ jerryscript/tests/jerry/numeric-separator.js
@@ -12,34 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 // few valid numeric separators
-assert (1_1 == 11)
-assert (0x11_11 == 4369)
-assert (0b11_11 == 15)
-assert (0o11_11 == 585)
-assert (1_1n == 11n)
-assert (100_001.11_00 == 100001.11)
-assert (0x11_11n == 4369n)
+assert(1_1 == 11)
+assert(0x11_11 == 4369)
+assert(0b11_11 == 15)
+assert(0o11_11 == 585)
+assert(1_1n == 11n)
+assert(100_001.11_00 == 100001.11)
+assert(0x11_11n == 4369n)
 
 // checker functions
 
-function invalid_numeric_separator (expression)
-{
+function invalid_numeric_separator(expression) {
   try {
-    var a = eval (expression)
-    assert (false)
+    var a = eval(expression)
+    assert(false)
   } catch (e) {
-    assert (true)
+    assert(true)
   }
 }
 
 // few invalid numeric separators
-invalid_numeric_separator ("_1_1")
-invalid_numeric_separator ("1__1")
-invalid_numeric_separator ("1_1_n")
-invalid_numeric_separator ("1_1n_")
-invalid_numeric_separator ("0b_11")
-invalid_numeric_separator ("0x_11")
-invalid_numeric_separator ("0o_11")
-invalid_numeric_separator ("0_0.123")
+invalid_numeric_separator("_1_1")
+invalid_numeric_separator("1__1")
+invalid_numeric_separator("1_1_n")
+invalid_numeric_separator("1_1n_")
+invalid_numeric_separator("0b_11")
+invalid_numeric_separator("0x_11")
+invalid_numeric_separator("0o_11")
+invalid_numeric_separator("0_0.123")
