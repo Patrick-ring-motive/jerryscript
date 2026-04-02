3927ddbba0f03d978509bb3e6df86881ed6b968c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:56:55 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/continue.js)

--- jerryscript/tests/jerry/continue.js
+++ jerryscript/tests/jerry/continue.js
@@ -13,32 +13,28 @@
  * limitations under the License.
  */
 
-for (var i = 0; i < 1000; i++)
-{
-  switch (1)
-  {
-  default:
-    /* This block must not be enclosed in braces. */
-    let j = eval();
-    continue;
+for (var i = 0; i < 1000; i++) {
+  switch (1) {
+    default:
+      /* This block must not be enclosed in braces. */
+      let j = eval();
+      continue;
   }
 }
 
 next:
-for (var i = 0; i < 1000; i++)
-{
-  for (let j = eval(); true; )
-  {
-    continue next;
+  for (var i = 0; i < 1000; i++) {
+    for (let j = eval(); true;) {
+      continue next;
+    }
   }
-}
 
 next:
-for (var i = 0; i < 1000; i++)
-{
-  for (let j in {a:1})
-  {
-    eval()
-    continue next;
+  for (var i = 0; i < 1000; i++) {
+    for (let j in {
+        a: 1
+      }) {
+      eval()
+      continue next;
+    }
   }
-}
