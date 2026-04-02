1250ea83b8b9a6004eb0a474e14d7e3479bdc64a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:57:17 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/date-tojson.js)

--- jerryscript/tests/jerry/date-tojson.js
+++ jerryscript/tests/jerry/date-tojson.js
@@ -13,8 +13,12 @@
 // limitations under the License.
 
 var obj = {
-    toISOString: function() { return "RESULT-ToISOString"; },
-    valueOf: function() { return "RESULT-valueOf"; }
+  toISOString: function() {
+    return "RESULT-ToISOString";
+  },
+  valueOf: function() {
+    return "RESULT-valueOf";
+  }
 };
 
 var result = Date.prototype.toJSON.call(obj);
