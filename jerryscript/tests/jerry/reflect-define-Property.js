cf8c76137e89a5737dc25c45f9ee732a8124d6cf
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:13:37 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/reflect-define-Property.js)

--- jerryscript/tests/jerry/reflect-define-Property.js
+++ jerryscript/tests/jerry/reflect-define-Property.js
@@ -13,11 +13,23 @@
 // limitations under the License.
 
 var obj = {};
-assert(Reflect['defineProperty'](obj, 'x', {value: 7}) === true);
-assert(Reflect['defineProperty'](obj, 'y', {value: function() {throw 5}}) === true);
+assert(Reflect['defineProperty'](obj, 'x', {
+  value: 7
+}) === true);
+assert(Reflect['defineProperty'](obj, 'y', {
+  value: function() {
+    throw 5
+  }
+}) === true);
 try {
-    Reflect['defineProperty'](obj, {toString: function() {throw new TypeError(5)}}, {value: 8});
-    assert(false);
+  Reflect['defineProperty'](obj, {
+    toString: function() {
+      throw new TypeError(5)
+    }
+  }, {
+    value: 8
+  });
+  assert(false);
 } catch (e) {
-    assert(e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
