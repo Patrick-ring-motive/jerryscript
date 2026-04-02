13eb43bb4eb7c2db112309c44ed54127fc4e534e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:15:46 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-1286.js)

--- jerryscript/tests/jerry/regression-test-issue-1286.js
+++ jerryscript/tests/jerry/regression-test-issue-1286.js
@@ -16,10 +16,10 @@ var builtinEval = eval;
 var eval = builtinEval.bind(undefined, "context");
 
 var context = "global";
+
 function checkIfDirectEval() {
   var context = "function";
   return (eval() == "function");
 }
 
-assert (!checkIfDirectEval());
-
+assert(!checkIfDirectEval());
