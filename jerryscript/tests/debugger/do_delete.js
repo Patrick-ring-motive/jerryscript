882a89ec79811f0294217d5a83a6201d4ebe78e5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:52:18 -0500

    style: js-beautify formatting (jerryscript/tests/debugger/do_delete.js)

--- jerryscript/tests/debugger/do_delete.js
+++ jerryscript/tests/debugger/do_delete.js
@@ -18,9 +18,9 @@ var x = 10;
 var y = 10;
 var pi = 3.14;
 
-function delete_test(x,y) {
-	var sum = x + y;
-	return
+function delete_test(x, y) {
+  var sum = x + y;
+  return
 }
 
 delete_test(x, y);
