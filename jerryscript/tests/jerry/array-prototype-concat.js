3960c5e0115ba60bf3766c72900bd65d823e4109
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:53:32 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-prototype-concat.js)

--- jerryscript/tests/jerry/array-prototype-concat.js
+++ jerryscript/tests/jerry/array-prototype-concat.js
@@ -20,7 +20,9 @@ for (i = 0; i < array.length; i++) {
   assert(array[i] === new_arr[i]);
 }
 
-var obj = { concat : Array.prototype.concat };
+var obj = {
+  concat: Array.prototype.concat
+};
 var arr1 = ["Apple", 6, "Peach"];
 var arr2 = [obj, "Cherry", "Grape"];
 
@@ -43,11 +45,11 @@ assert(new_array[5] === "Grape");
 assert(new_array[6] === obj);
 assert(new_array[7] === 1);
 
-var arr1 = [1,2];
-var arr2 = [4,5,6,7,8];
-var arr3 = [,,9,10];
+var arr1 = [1, 2];
+var arr2 = [4, 5, 6, 7, 8];
+var arr3 = [, , 9, 10];
 var arr4 = [];
-var expected = [1,2,4,5,6,7,8,,,9,10];
+var expected = [1, 2, 4, 5, 6, 7, 8, , , 9, 10];
 
 var result = arr1.concat(arr2, arr3, arr4);
 
@@ -64,7 +66,11 @@ assert(arr1.concat(arr2).length === arr1.length + arr2.length);
 
 // Checking behavior when unable to get element
 var arr = []
-Object.defineProperty(arr, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(arr, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 arr.length = 1;
 
 try {
@@ -80,7 +86,11 @@ try {
 arr1 = [];
 arr2 = [];
 arr3 = [];
-Object.defineProperty(arr2, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(arr2, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 
 try {
   arr1.concat(arr2, arr3);
