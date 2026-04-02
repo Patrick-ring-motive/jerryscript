d2735022fadc23c00503e5569b75f6a5f380d7fd
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:18:08 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-255.js)

--- jerryscript/tests/jerry/regression-test-issue-255.js
+++ jerryscript/tests/jerry/regression-test-issue-255.js
@@ -14,12 +14,9 @@
 
 v_0 = /(?!(?!l{666,}))/;
 
-try
-{
+try {
   v_0.exec("llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll");
-}
-catch (e)
-{
-  assert (e instanceof RangeError);
-  assert (e.message === "RegExp executor steps limit is exceeded.");
+} catch (e) {
+  assert(e instanceof RangeError);
+  assert(e.message === "RegExp executor steps limit is exceeded.");
 }
