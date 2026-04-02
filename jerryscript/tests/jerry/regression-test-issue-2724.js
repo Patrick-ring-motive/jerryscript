4d09d1504f5c5d34e84994104e247d77dd63cf89
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:19:05 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-2724.js)

--- jerryscript/tests/jerry/regression-test-issue-2724.js
+++ jerryscript/tests/jerry/regression-test-issue-2724.js
@@ -13,10 +13,16 @@
 // limitations under the License.
 
 try {
-  var obj = { reverse : Int32Array.prototype.join } ;
-  Object.defineProperty( obj, 'length', { 'get' : function(toPrecision) { return Object.create(/x/); }});
+  var obj = {
+    reverse: Int32Array.prototype.join
+  };
+  Object.defineProperty(obj, 'length', {
+    'get': function(toPrecision) {
+      return Object.create(/x/);
+    }
+  });
   obj.reverse("$01$02$11$20");
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
