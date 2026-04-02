c41ec58e9ca0331b9e27d17ce4dc8f701f91def7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:54:35 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-prototype-tolocalestring.js)

--- jerryscript/tests/jerry/array-prototype-tolocalestring.js
+++ jerryscript/tests/jerry/array-prototype-tolocalestring.js
@@ -12,20 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-assert ([].toLocaleString() === "");
-assert ([1].toLocaleString() === "1");
-assert ([1,2].toLocaleString() === "1,2");
-assert ([1,2,3].toLocaleString() === "1,2,3");
+assert([].toLocaleString() === "");
+assert([1].toLocaleString() === "1");
+assert([1, 2].toLocaleString() === "1,2");
+assert([1, 2, 3].toLocaleString() === "1,2,3");
 
 var test_ok = {
   length: 1,
-  toLocaleString: function() { return "1"; }
+  toLocaleString: function() {
+    return "1";
+  }
 };
 
-assert ([3, test_ok, 4, test_ok].toLocaleString() === "3,1,4,1");
+assert([3, test_ok, 4, test_ok].toLocaleString() === "3,1,4,1");
 
-
-var obj = { toLocaleString: function() {} };
+var obj = {
+  toLocaleString: function() {}
+};
 var test_non_str_locale = [undefined, obj, null, obj, obj];
 
 assert(test_non_str_locale.toLocaleString() === ",undefined,,undefined,undefined");
@@ -36,21 +39,21 @@ var test_fail = {
 
 try {
   [test_fail].toLocaleString();
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
 
-
 var test_fail_call = {
-  toLocaleString: function() { throw new ReferenceError("foo"); }
+  toLocaleString: function() {
+    throw new ReferenceError("foo");
+  }
 };
-
 
 try {
   [1, 2, test_fail_call].toLocaleString();
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e.message === "foo");
-  assert (e instanceof ReferenceError);
+  assert(e.message === "foo");
+  assert(e instanceof ReferenceError);
 }
