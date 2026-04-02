ae22b26b65dc3437b4b7a95b99d64b5b09dc817a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:11:48 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/parser-oom.js)

--- jerryscript/tests/jerry/parser-oom.js
+++ jerryscript/tests/jerry/parser-oom.js
@@ -24,16 +24,12 @@ str = "(function() { return " + str + "1 })";
 /* Eat memory. */
 var array = [];
 
-try
-{
-  for (var i = 0; i < 30; i++)
-  {
+try {
+  for (var i = 0; i < 30; i++) {
     array[i] = eval(str);
   }
-  assert (false);
-}
-catch (err)
-{
+  assert(false);
+} catch (err) {
   array = null;
-  assert (err === null);
+  assert(err === null);
 }
