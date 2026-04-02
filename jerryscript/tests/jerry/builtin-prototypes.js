23694850736410b9b39b6ea5e8391045fac30e62
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:55:32 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/builtin-prototypes.js)

--- jerryscript/tests/jerry/builtin-prototypes.js
+++ jerryscript/tests/jerry/builtin-prototypes.js
@@ -13,57 +13,57 @@
  * limitations under the License.
  */
 
-assert (Object.prototype.toString.call (String.prototype) === '[object String]');
-assert (String.prototype.toString() === "");
+assert(Object.prototype.toString.call(String.prototype) === '[object String]');
+assert(String.prototype.toString() === "");
 
-assert (Object.prototype.toString.call (Boolean.prototype) === '[object Boolean]');
-assert (Boolean.prototype.valueOf() === false);
+assert(Object.prototype.toString.call(Boolean.prototype) === '[object Boolean]');
+assert(Boolean.prototype.valueOf() === false);
 
-assert (Object.prototype.toString.call (Number.prototype) === '[object Number]');
-assert (Number.prototype.valueOf() === 0);
+assert(Object.prototype.toString.call(Number.prototype) === '[object Number]');
+assert(Number.prototype.valueOf() === 0);
 
 var prototypes = [
-    Date.prototype,
-    RegExp.prototype,
-    Error.prototype,
-    EvalError.prototype,
-    RangeError.prototype,
-    ReferenceError.prototype,
-    SyntaxError.prototype,
-    TypeError.prototype,
-    URIError.prototype
-  ]
+  Date.prototype,
+  RegExp.prototype,
+  Error.prototype,
+  EvalError.prototype,
+  RangeError.prototype,
+  ReferenceError.prototype,
+  SyntaxError.prototype,
+  TypeError.prototype,
+  URIError.prototype
+]
 
 for (proto of prototypes) {
-  assert (Object.prototype.toString.call (proto) === '[object Object]');
+  assert(Object.prototype.toString.call(proto) === '[object Object]');
 }
 
 try {
   Date.prototype.valueOf();
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
 
 try {
   RegExp.prototype.exec("");
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
 
 try {
   RegExp.prototype.compile("a", "u");
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
 
-assert (RegExp.prototype.source === '(?:)');
-assert (RegExp.prototype.global === undefined);
-assert (RegExp.prototype.ignoreCase === undefined);
-assert (RegExp.prototype.multiline === undefined);
-assert (RegExp.prototype.sticky === undefined);
-assert (RegExp.prototype.unicode === undefined);
-assert (RegExp.prototype.dotAll === undefined);
-assert (RegExp.prototype.flags === '');
+assert(RegExp.prototype.source === '(?:)');
+assert(RegExp.prototype.global === undefined);
+assert(RegExp.prototype.ignoreCase === undefined);
+assert(RegExp.prototype.multiline === undefined);
+assert(RegExp.prototype.sticky === undefined);
+assert(RegExp.prototype.unicode === undefined);
+assert(RegExp.prototype.dotAll === undefined);
+assert(RegExp.prototype.flags === '');
