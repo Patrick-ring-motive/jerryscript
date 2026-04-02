7462750beced131fe280992de8a69b58f34d5c73
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:01:03 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/regression-test-issue-3131.js)

--- jerryscript/tests/jerry/fail/regression-test-issue-3131.js
+++ jerryscript/tests/jerry/fail/regression-test-issue-3131.js
@@ -12,4 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-switch ( $) { case $: for ( ; ; ) ; case $ :}
+switch ($) {
+  case $:
+    for (;;);
+  case $:
+}
