bb5eeb7cfe4eece0065f9fdd336677e5d38286a7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:12:18 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/promise-all-iterator.js)

--- jerryscript/tests/jerry/promise-all-iterator.js
+++ jerryscript/tests/jerry/promise-all-iterator.js
@@ -14,7 +14,7 @@
  */
 
 function createIterable(arr, methods = {}) {
-  let iterable = function *() {
+  let iterable = function*() {
     let idx = 0;
     while (idx < arr.length) {
       yield arr[idx];
@@ -28,7 +28,14 @@ function createIterable(arr, methods = {}) {
 };
 
 // iterator next
-Promise.all({[Symbol.iterator]() { return {get next() { throw 5; }}}
+Promise.all({
+  [Symbol.iterator]() {
+    return {
+      get next() {
+        throw 5;
+      }
+    }
+  }
 }).then(onfullfilled => {
   // If this is not called, the promise has failed, and catch must be called.
   assert(false);
@@ -37,7 +44,18 @@ Promise.all({[Symbol.iterator]() { return {get next() { throw 5; }}}
 });
 
 // iterator value
-Promise.all({ [Symbol.iterator] () { return { next () { return { get value () { throw 5 }}}}}
+Promise.all({
+  [Symbol.iterator]() {
+    return {
+      next() {
+        return {
+          get value() {
+            throw 5
+          }
+        }
+      }
+    }
+  }
 }).then(onfullfilled => {
   // If this is not called, the promise has failed, and catch must be called.
   assert(false);
@@ -46,7 +64,18 @@ Promise.all({ [Symbol.iterator] () { return { next () { return { get value () { 
 });
 
 // iterator done
-Promise.all({ [Symbol.iterator] () { return { next () { return { get done () { throw 5 }}}}}
+Promise.all({
+  [Symbol.iterator]() {
+    return {
+      next() {
+        return {
+          get done() {
+            throw 5
+          }
+        }
+      }
+    }
+  }
 }).then(onfullfilled => {
   // If this is not called, the promise has failed, and catch must be called.
   assert(false);
@@ -55,7 +84,10 @@ Promise.all({ [Symbol.iterator] () { return { next () { return { get done () { t
 });
 
 // iterator get
-Promise.all({ get [Symbol.iterator] () { throw 5 }
+Promise.all({
+  get [Symbol.iterator]() {
+    throw 5
+  }
 }).then(onfullfilled => {
   // If this is not called, the promise has failed, and catch must be called.
   assert(false);
@@ -64,22 +96,42 @@ Promise.all({ get [Symbol.iterator] () { throw 5 }
 });
 
 var fulfills = Promise.all(createIterable([
-  new Promise(resolve => { resolve("foo"); }),
-  new Promise(resolve => { resolve("bar"); }),
+  new Promise(resolve => {
+    resolve("foo");
+  }),
+  new Promise(resolve => {
+    resolve("bar");
+  }),
 ]));
 var rejects = Promise.all(createIterable([
-  new Promise((_, reject) => { reject("baz"); }),
-  new Promise((_, reject) => { reject("qux"); }),
+  new Promise((_, reject) => {
+    reject("baz");
+  }),
+  new Promise((_, reject) => {
+    reject("qux");
+  }),
 ]));
 
-fulfills.then(result => { assert (result + "" === "foo,bar"); });
-rejects.catch(result => { assert (result === "baz"); });
+fulfills.then(result => {
+  assert(result + "" === "foo,bar");
+});
+rejects.catch(result => {
+  assert(result === "baz");
+});
 
 var closed = true;
 delete Promise.resolve;
-Promise.all(createIterable([1,2,3], {'return': function () { closed = false; }}));
-assert (!closed);
+Promise.all(createIterable([1, 2, 3], {
+  'return': function() {
+    closed = false;
+  }
+}));
+assert(!closed);
 
 var arr = [];
-Object.defineProperty(arr, Symbol.species, { get: function () { assert(false) }});
+Object.defineProperty(arr, Symbol.species, {
+  get: function() {
+    assert(false)
+  }
+});
 Promise.all(arr);
