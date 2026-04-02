3984d9f7f45442b6f1df90400ca3b363df9b399d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:55:14 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/bigint5.js)

--- jerryscript/tests/jerry/bigint5.js
+++ jerryscript/tests/jerry/bigint5.js
@@ -13,8 +13,7 @@
  * limitations under the License.
  */
 
-function check_syntax_error(code)
-{
+function check_syntax_error(code) {
   try {
     eval(code)
     assert(false)
@@ -45,10 +44,16 @@ assert(0xffn == 255n)
 assert(0o77777n == 0x7fffn)
 assert(255n.toString(16) == "ff")
 
-var o = { 12n : "data" }
+var o = {
+  12n: "data"
+}
 assert(o[12] === "data")
 
-var c = class C { static 19n () { return "BigInt" } }
+var c = class C {
+  static 19n() {
+    return "BigInt"
+  }
+}
 assert(c[19]() === "BigInt")
 
 function f(p, q) {
