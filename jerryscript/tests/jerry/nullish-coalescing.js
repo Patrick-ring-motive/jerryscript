da973a0cc5dacbf37a0b27c80aaf9768e257be8c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:08:53 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/nullish-coalescing.js)

--- jerryscript/tests/jerry/nullish-coalescing.js
+++ jerryscript/tests/jerry/nullish-coalescing.js
@@ -18,24 +18,24 @@ assert((0 ?? 2) == 0)
 assert(null ?? 2 == 2)
 assert(null ?? undefined == undefined)
 assert(null ?? undefined ?? 2 == 2)
-assert(null ?? undefined ?? null ?? 10  == 10)
-assert(null ?? (undefined || null) ?? 10  == 10)
-assert(null ?? (undefined && null) ?? 10  == 10)
+assert(null ?? undefined ?? null ?? 10 == 10)
+assert(null ?? (undefined || null) ?? 10 == 10)
+assert(null ?? (undefined && null) ?? 10 == 10)
 assert((null ?? true) && (true ?? null) == true)
 
 //cannot evulate the right expression if left is not null or undefined
-function poison () {
+function poison() {
   throw 23;
 }
-assert(true ?? poison ())
-assert(null ?? null ?? true ?? poison ())
+assert(true ?? poison())
+assert(null ?? null ?? true ?? poison())
 
-function checkSyntax (str) {
+function checkSyntax(str) {
   try {
-    eval (str);
-    assert (false);
+    eval(str);
+    assert(false);
   } catch (e) {
-    assert (e instanceof SyntaxError);
+    assert(e instanceof SyntaxError);
   }
 }
 
@@ -45,7 +45,7 @@ var headNullish2 = "(null ?? null && null )";
 var tailNullish1 = "(null || null ?? null )";
 var tailNullish2 = "(null || null ?? null )";
 
-checkSyntax (headNullish1);
-checkSyntax (headNullish2);
-checkSyntax (tailNullish1);
-checkSyntax (tailNullish2);
+checkSyntax(headNullish1);
+checkSyntax(headNullish2);
+checkSyntax(tailNullish1);
+checkSyntax(tailNullish2);
