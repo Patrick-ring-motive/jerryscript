304110649d89cca2c110b06cfae18bc2b87ac5f7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:03:39 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/function-pattern1.js)

--- jerryscript/tests/jerry/function-pattern1.js
+++ jerryscript/tests/jerry/function-pattern1.js
@@ -12,27 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function must_throw (str, type = SyntaxError)
-{
-  try
-  {
-    eval (str);
-    assert (false);
-  }
-  catch (e)
-  {
-    assert (e instanceof type)
+function must_throw(str, type = SyntaxError) {
+  try {
+    eval(str);
+    assert(false);
+  } catch (e) {
+    assert(e instanceof type)
   }
 }
 
-must_throw ("function f(a, [a]) {}");
-must_throw ("function f([a], a) {}");
-must_throw ("function f(a = b, [b]) {}; f()", ReferenceError);
-must_throw ("function f([a+b]) {}");
-must_throw ("function f([a().b]) {}");
-must_throw ("function f(...[a] = [1]) {}");
+must_throw("function f(a, [a]) {}");
+must_throw("function f([a], a) {}");
+must_throw("function f(a = b, [b]) {}; f()", ReferenceError);
+must_throw("function f([a+b]) {}");
+must_throw("function f([a().b]) {}");
+must_throw("function f(...[a] = [1]) {}");
 
-function a1([a,b]) {
+function a1([a, b]) {
   var a, b;
 
   assert(a === 1);
@@ -43,7 +39,7 @@ function a1([a,b]) {
 }
 a1([1]);
 
-function a2([a,b]) {
+function a2([a, b]) {
   eval("var a, b");
   assert(a === 1);
   assert(b === undefined);
@@ -53,42 +49,40 @@ function a2([a,b]) {
 }
 a2([1]);
 
-function f1(a, [b], c, [d], e)
-{
-  assert (a === 1);
-  assert (b === 2);
-  assert (c === 3);
-  assert (d === 4);
-  assert (e === 5);
+function f1(a, [b], c, [d], e) {
+  assert(a === 1);
+  assert(b === 2);
+  assert(c === 3);
+  assert(d === 4);
+  assert(e === 5);
 }
 f1(1, [2], 3, [4], 5)
 
-function f2(a, [b], c, [d], e)
-{
+function f2(a, [b], c, [d], e) {
   eval("");
-  assert (a === 1);
-  assert (b === 2);
-  assert (c === 3);
-  assert (d === 4);
-  assert (e === 5);
+  assert(a === 1);
+  assert(b === 2);
+  assert(c === 3);
+  assert(d === 4);
+  assert(e === 5);
 }
 f2(1, [2], 3, [4], 5)
 
 var g1 = (a, [b], c, [d], e) => {
-  assert (a === 1);
-  assert (b === 2);
-  assert (c === 3);
-  assert (d === 4);
-  assert (e === 5);
+  assert(a === 1);
+  assert(b === 2);
+  assert(c === 3);
+  assert(d === 4);
+  assert(e === 5);
 }
 g1(1, [2], 3, [4], 5)
 
 var g2 = (a, [b], c, [d], e) => {
   eval("");
-  assert (a === 1);
-  assert (b === 2);
-  assert (c === 3);
-  assert (d === 4);
-  assert (e === 5);
+  assert(a === 1);
+  assert(b === 2);
+  assert(c === 3);
+  assert(d === 4);
+  assert(e === 5);
 }
 g2(1, [2], 3, [4], 5)
