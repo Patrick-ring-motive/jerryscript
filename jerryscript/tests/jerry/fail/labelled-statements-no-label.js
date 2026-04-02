a39f6e20179b11d5c9f7ab1c1e358d584398fe1f
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:58:25 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/labelled-statements-no-label.js)

--- jerryscript/tests/jerry/fail/labelled-statements-no-label.js
+++ jerryscript/tests/jerry/fail/labelled-statements-no-label.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-for (var i = 0; i < 10; i++)
-{
+for (var i = 0; i < 10; i++) {
   break a;
 }
