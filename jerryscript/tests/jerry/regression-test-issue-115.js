f01d7116014586ebce8e5902c009bf5983d21c28
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:15:29 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-115.js)

--- jerryscript/tests/jerry/regression-test-issue-115.js
+++ jerryscript/tests/jerry/regression-test-issue-115.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var v_1 = [,];
+var v_1 = [, ];
 Object.defineProperty(v_1, "0", {
-    set: function() {},
+  set: function() {},
 });
