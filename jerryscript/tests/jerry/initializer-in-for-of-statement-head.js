7748a1dd5a3953f7927b866e37a8f7eb197505a7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:05:05 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/initializer-in-for-of-statement-head.js)

--- jerryscript/tests/jerry/initializer-in-for-of-statement-head.js
+++ jerryscript/tests/jerry/initializer-in-for-of-statement-head.js
@@ -14,12 +14,12 @@
  */
 
 function parse(txt) {
-    try {
-        eval(txt)
-        assert(false)
-    } catch (e) {
-        assert(e instanceof SyntaxError)
-    }
+  try {
+    eval(txt)
+    assert(false)
+  } catch (e) {
+    assert(e instanceof SyntaxError)
+  }
 }
 
 parse("for (var x = 1 of []) { }")
