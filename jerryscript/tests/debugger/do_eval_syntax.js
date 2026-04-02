dc5c2babcfbe887236ef3d2228eb51189aecf332
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:52:30 -0500

    style: js-beautify formatting (jerryscript/tests/debugger/do_eval_syntax.js)

--- jerryscript/tests/debugger/do_eval_syntax.js
+++ jerryscript/tests/debugger/do_eval_syntax.js
@@ -12,13 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function foo(loop)
-{
-    while(loop)
-    {
-        print("eval");
-        print("syntax");
-    }
+function foo(loop) {
+  while (loop) {
+    print("eval");
+    print("syntax");
+  }
 }
 
 var a = 10;
