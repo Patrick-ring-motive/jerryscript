50b0dc922ee0edc17613e2fb76eafb7a581b0778
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:16:14 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-1616.js)

--- jerryscript/tests/jerry/regression-test-issue-1616.js
+++ jerryscript/tests/jerry/regression-test-issue-1616.js
@@ -14,14 +14,10 @@
 
 var name = "";
 
-try
-{
-  var a = new ArrayBuffer (0xfffffffe)
-}
-catch (e)
-{
+try {
+  var a = new ArrayBuffer(0xfffffffe)
+} catch (e) {
   name = e.name;
 }
 
 assert(name === "RangeError");
-
