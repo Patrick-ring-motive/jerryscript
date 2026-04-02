13ad632cffeb962a3328268706d87dc80b502fb9
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:01:45 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/regression-test-issue-358.js)

--- jerryscript/tests/jerry/fail/regression-test-issue-358.js
+++ jerryscript/tests/jerry/fail/regression-test-issue-358.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-label_0: 
-    if (false)
-        do  {
-            continue label_0;
-        } while (false);
+label_0:
+  if (false)
+    do {
+      continue label_0;
+    } while (false);
