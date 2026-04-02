a1260169cd1a5976c17b67e53617f0f49a4247e9
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:02:20 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/for-let.js)

--- jerryscript/tests/jerry/for-let.js
+++ jerryscript/tests/jerry/for-let.js
@@ -13,27 +13,28 @@
 // limitations under the License.
 
 var arr = [];
-for (let i = 0, j = 0; i < 10; j++)
-{
-  arr[i] = function() { return i + j; }
+for (let i = 0, j = 0; i < 10; j++) {
+  arr[i] = function() {
+    return i + j;
+  }
   i++;
 }
 
 for (let i = 0; i < 10; i++)
   assert(arr[i]() === (i * 2 + 1));
 
-var j = 0, k = 0;
-for (let i = 0; j = i, i < 10; i++)
-{
+var j = 0,
+  k = 0;
+for (let i = 0; j = i, i < 10; i++) {
   let i = -3;
   assert(i === -3);
   assert(j === k);
   k++;
 }
 
-var j = 0, k = 0;
-for (let i = 0; eval("j = i"), i < 10; i++)
-{
+var j = 0,
+  k = 0;
+for (let i = 0; eval("j = i"), i < 10; i++) {
   let i = -3;
   assert(i === -3);
   assert(j === k);
@@ -41,10 +42,15 @@ for (let i = 0; eval("j = i"), i < 10; i++)
 }
 
 var arr = [];
-for (let i in { x:1, y:1, z:1 })
-{
+for (let i in {
+    x: 1,
+    y: 1,
+    z: 1
+  }) {
   let str = "P";
-  arr.push(function () { return str + i; });
+  arr.push(function() {
+    return str + i;
+  });
 }
 
 assert(arr[0]() === "Px");
@@ -52,17 +58,17 @@ assert(arr[1]() === "Py");
 assert(arr[2]() === "Pz");
 
 try {
-  for (let i in (function() { return i; })()) {}
+  for (let i in (function() {
+      return i;
+    })()) {}
   assert(false);
 } catch (e) {
   assert(e instanceof ReferenceError);
 }
 
 try {
-  for (let i = 0, j = 0; i < 5; i++, j++)
-  {
-    if (i === 3)
-    {
+  for (let i = 0, j = 0; i < 5; i++, j++) {
+    if (i === 3) {
       eval("throw -42")
     }
   }
@@ -72,8 +78,7 @@ try {
 }
 
 exit: {
-  for (let i = 0, j = 0; i < 5; i++, j++)
-  {
+  for (let i = 0, j = 0; i < 5; i++, j++) {
     if (eval("i === 3")) {
       assert(i === 3);
       break exit;
@@ -82,22 +87,32 @@ exit: {
   assert(false);
 }
 
-var f = null, g = null, h = null;
+var f = null,
+  g = null,
+  h = null;
 
-for (let i = 0;
-     f = function() { return i }, i < 1;
-     i++, g = function() { return i })
-{
-  h = function() { return i };
+for (let i = 0; f = function() {
+    return i
+  }, i < 1; i++, g = function() {
+    return i
+  }) {
+  h = function() {
+    return i
+  };
 }
 assert(f() === 1);
 assert(g() === 1);
 assert(h() === 0);
 
 var arr = [];
-for (const i in { aa:4, bb:5, cc:6 })
-{
-  arr.push(function () { return i });
+for (const i in {
+    aa: 4,
+    bb: 5,
+    cc: 6
+  }) {
+  arr.push(function() {
+    return i
+  });
 }
 
 assert(arr[0]() === "aa");
