859154f57da0b943f735c9928eed25faab804ac5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:06:38 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/math-functions-tonumber-rule.js)

--- jerryscript/tests/jerry/math-functions-tonumber-rule.js
+++ jerryscript/tests/jerry/math-functions-tonumber-rule.js
@@ -13,21 +13,36 @@
 // limitations under the License.
 
 var str = "";
-var a = {valueOf: function() { str += "a"; return 1;}};
-var b = {valueOf: function() { str += "b"; return NaN;}};
-var c = {valueOf: function() { str += "c"; return 2;}};
+var a = {
+  valueOf: function() {
+    str += "a";
+    return 1;
+  }
+};
+var b = {
+  valueOf: function() {
+    str += "b";
+    return NaN;
+  }
+};
+var c = {
+  valueOf: function() {
+    str += "c";
+    return 2;
+  }
+};
 
-Math.min (a, b, c);
-assert (str === "abc");
+Math.min(a, b, c);
+assert(str === "abc");
 
 str = "";
-Math.max (a, b, c);
-assert (str === "abc");
+Math.max(a, b, c);
+assert(str === "abc");
 
 str = "";
-Math.pow(b,a);
-assert (str === "ba");
+Math.pow(b, a);
+assert(str === "ba");
 
 str = "";
-Math.atan2(b,a);
-assert (str === "ba");
+Math.atan2(b, a);
+assert(str === "ba");
