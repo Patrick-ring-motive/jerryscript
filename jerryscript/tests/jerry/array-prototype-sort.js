c3aa818ebf2b0f1483cfb18c51b871ec29929db7
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:54:30 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-prototype-sort.js)

--- jerryscript/tests/jerry/array-prototype-sort.js
+++ jerryscript/tests/jerry/array-prototype-sort.js
@@ -48,14 +48,14 @@ for (i = 0; i < array.length; i++) {
 }
 
 // Sorting sparse array
-var array = [1,,2,,3,,4,undefined,5];
-var expected = [1,2,3,4,5,undefined,,,,];
+var array = [1, , 2, , 3, , 4, undefined, 5];
+var expected = [1, 2, 3, 4, 5, undefined, , , , ];
 
 array.sort();
 
 assert(array.length === expected.length);
 for (i = 0; i < array.length; i++) {
-  assert(expected.hasOwnProperty (i) === array.hasOwnProperty (i));
+  assert(expected.hasOwnProperty(i) === array.hasOwnProperty(i));
   assert(array[i] === expected[i]);
 }
 
@@ -70,8 +70,14 @@ try {
 }
 
 // Checking behavior when unable to get length
-var obj = { sort : Array.prototype.sort}
-Object.defineProperty(obj, 'length', { 'get' : function () { throw new ReferenceError ("foo"); } });
+var obj = {
+  sort: Array.prototype.sort
+}
+Object.defineProperty(obj, 'length', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 
 try {
   obj.sort();
@@ -82,8 +88,15 @@ try {
 }
 
 // Checking behavior when unable to get element
-var obj = { sort : Array.prototype.sort, length : 1}
-Object.defineProperty(obj, '0', { 'get' : function () { throw new ReferenceError ("foo"); } });
+var obj = {
+  sort: Array.prototype.sort,
+  length: 1
+}
+Object.defineProperty(obj, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 
 try {
   obj.sort();
@@ -94,9 +107,20 @@ try {
 }
 
 // Checking behavior when unable to get elements
-var obj = { sort : Array.prototype.sort, length : 2};
-Object.defineProperty(obj, '0', { 'get' : function () { throw new ReferenceError ("foo"); } });
-Object.defineProperty(obj, '1', { 'get' : function () { throw new ReferenceError ("bar"); } });
+var obj = {
+  sort: Array.prototype.sort,
+  length: 2
+};
+Object.defineProperty(obj, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
+Object.defineProperty(obj, '1', {
+  'get': function() {
+    throw new ReferenceError("bar");
+  }
+});
 
 try {
   obj.sort();
@@ -110,15 +134,22 @@ try {
 var arr = [1, 0];
 
 try {
-  arr.sort(function () { Object.freeze(arr) });
+  arr.sort(function() {
+    Object.freeze(arr)
+  });
   assert(false);
 } catch (e) {
   assert(e instanceof TypeError);
 }
 
 // Checking behavior when unable to delete property
-var obj = {sort : Array.prototype.sort, '0' : 2, '1' : 1, length : 4};
-Object.defineProperty(obj, '3', function () {});
+var obj = {
+  sort: Array.prototype.sort,
+  '0': 2,
+  '1': 1,
+  length: 4
+};
+Object.defineProperty(obj, '3', function() {});
 
 try {
   obj.sort();
@@ -129,7 +160,11 @@ try {
 
 // Checking behavior when unable to get the last element
 var arr = [1, 2, ];
-Object.defineProperty(arr, '2', { 'get' : function () { throw new ReferenceError ("foo"); } });
+Object.defineProperty(arr, '2', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 
 try {
   arr.sort();
@@ -140,8 +175,12 @@ try {
 }
 
 // Checking behavior when lhs_value throws exception at comparefn
-f = function () { throw new ReferenceError('foo'); };
-obj = { 'toString' : f };
+f = function() {
+  throw new ReferenceError('foo');
+};
+obj = {
+  'toString': f
+};
 arr = [obj, 1];
 
 try {
@@ -153,8 +192,12 @@ try {
 }
 
 // Checking behavior when rhs_value throws exception at comparefn
-f = function () { throw new ReferenceError('foo'); };
-obj = { 'toString' : f };
+f = function() {
+  throw new ReferenceError('foo');
+};
+obj = {
+  'toString': f
+};
 arr = [1, obj];
 
 try {
@@ -173,9 +216,11 @@ assert(arr[0] === 'foo');
 assert(arr[1] === 'foo');
 
 // Checking behavior when comparefn's call value cannot be converted to number
-obj = { };
-Object.defineProperty(obj, 'toString', function () { });
-f = function () { return obj };
+obj = {};
+Object.defineProperty(obj, 'toString', function() {});
+f = function() {
+  return obj
+};
 arr = [1, 2];
 
 try {
@@ -185,8 +230,12 @@ try {
   assert(e instanceof TypeError);
 }
 
-var proxy = new Proxy({length: 5}, {
-  getOwnPropertyDescriptor() { throw 42.5; }
+var proxy = new Proxy({
+  length: 5
+}, {
+  getOwnPropertyDescriptor() {
+    throw 42.5;
+  }
 })
 
 try {
