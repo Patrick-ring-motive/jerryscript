c9b6148f0fab8019f17399d70efa565d24e7e410
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:17:08 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-2143.js)

--- jerryscript/tests/jerry/regression-test-issue-2143.js
+++ jerryscript/tests/jerry/regression-test-issue-2143.js
@@ -15,7 +15,11 @@
 var a = new Uint8Array(2);
 
 try {
-  a[0] = { valueOf : function() { throw "intoint"; } };
+  a[0] = {
+    valueOf: function() {
+      throw "intoint";
+    }
+  };
   assert(false);
 } catch (e) {
   assert(e === "intoint")
