629246c9e30fc346d37f8237577796aa92d54fa5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:07:20 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/module-circular-08.mjs)

--- jerryscript/tests/jerry/module-circular-08.mjs
+++ jerryscript/tests/jerry/module-circular-08.mjs
@@ -12,4 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export {a as b} from "./module-circular-06.mjs"
+export {
+  a as b
+}
+from "./module-circular-06.mjs"
