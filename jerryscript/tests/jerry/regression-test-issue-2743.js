abc54bb8aa18b672bbf1bcb1cd650635c42386eb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:19:08 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-2743.js)

--- jerryscript/tests/jerry/regression-test-issue-2743.js
+++ jerryscript/tests/jerry/regression-test-issue-2743.js
@@ -17,8 +17,10 @@ var v2 = new RegExp();
 var v3 = new Array(v1)
 
 try {
-  var v4 = v3.forEach(function(p_0, p_1, p_2) { return p_0 + p_1 + p_2; }, v2);
-  assert (false);
+  var v4 = v3.forEach(function(p_0, p_1, p_2) {
+    return p_0 + p_1 + p_2;
+  }, v2);
+  assert(false);
 } catch (e) {
-  assert (e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
