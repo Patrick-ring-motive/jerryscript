b3b9df7cb9b9fe961745847d57d787140c36ce5a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:00:27 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/regression-test-issue-2901.js)

--- jerryscript/tests/jerry/fail/regression-test-issue-2901.js
+++ jerryscript/tests/jerry/fail/regression-test-issue-2901.js
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-object = new Object ( )
-var str = Symbol ( 'foo' )
-assert ( object + str === "[object Object]" )
+object = new Object()
+var str = Symbol('foo')
+assert(object + str === "[object Object]")
