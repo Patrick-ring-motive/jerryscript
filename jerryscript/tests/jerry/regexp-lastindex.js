ddfde2bbf81118187c852c75a7a4c2dd46a42edc
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:14:27 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regexp-lastindex.js)

--- jerryscript/tests/jerry/regexp-lastindex.js
+++ jerryscript/tests/jerry/regexp-lastindex.js
@@ -23,45 +23,45 @@ assert(RegExp.prototype.lastIndex === undefined)
 
 var r = /./y
 r.lastIndex = -1;
-assert (JSON.stringify(r.exec("abca")) === '["a"]');
-assert (r.lastIndex === 1);
-assert (JSON.stringify(r.exec("abca")) === '["b"]');
-assert (r.lastIndex === 2);
+assert(JSON.stringify(r.exec("abca")) === '["a"]');
+assert(r.lastIndex === 1);
+assert(JSON.stringify(r.exec("abca")) === '["b"]');
+assert(r.lastIndex === 2);
 
 r.lastIndex = 5;
-assert (JSON.stringify(r.exec("abca")) === 'null');
-assert (r.lastIndex === 0);
+assert(JSON.stringify(r.exec("abca")) === 'null');
+assert(r.lastIndex === 0);
 
 var r = /a/y
-assert (JSON.stringify(r.exec("abca")) === '["a"]');
-assert (r.lastIndex === 1);
-assert (JSON.stringify(r.exec("abca")) === 'null');
-assert (r.lastIndex === 0);
+assert(JSON.stringify(r.exec("abca")) === '["a"]');
+assert(r.lastIndex === 1);
+assert(JSON.stringify(r.exec("abca")) === 'null');
+assert(r.lastIndex === 0);
 
 var r = /./g
 r.lastIndex = -1;
-assert (JSON.stringify(r.exec("abca")) === '["a"]');
-assert (r.lastIndex === 1);
-assert (JSON.stringify(r.exec("abca")) === '["b"]');
-assert (r.lastIndex === 2);
+assert(JSON.stringify(r.exec("abca")) === '["a"]');
+assert(r.lastIndex === 1);
+assert(JSON.stringify(r.exec("abca")) === '["b"]');
+assert(r.lastIndex === 2);
 
 r.lastIndex = 5;
-assert (JSON.stringify(r.exec("abca")) === 'null');
-assert (r.lastIndex === 0);
+assert(JSON.stringify(r.exec("abca")) === 'null');
+assert(r.lastIndex === 0);
 
 var r = /a/g
-assert (JSON.stringify(r.exec("abca")) === '["a"]');
-assert (r.lastIndex === 1);
-assert (JSON.stringify(r.exec("abca")) === '["a"]');
-assert (r.lastIndex === 4);
-assert (JSON.stringify(r.exec("abca")) === 'null');
-assert (r.lastIndex === 0);
+assert(JSON.stringify(r.exec("abca")) === '["a"]');
+assert(r.lastIndex === 1);
+assert(JSON.stringify(r.exec("abca")) === '["a"]');
+assert(r.lastIndex === 4);
+assert(JSON.stringify(r.exec("abca")) === 'null');
+assert(r.lastIndex === 0);
 
 var r = /./uim
 r.lastIndex = 2;
-assert (JSON.stringify(r.exec("abcd")) === '["a"]');
-assert (r.lastIndex === 2);
+assert(JSON.stringify(r.exec("abcd")) === '["a"]');
+assert(r.lastIndex === 2);
 
 r.lastIndex = "lastIndex";
-assert (JSON.stringify(r.exec("abcd")) === '["a"]');
-assert (r.lastIndex === "lastIndex");
+assert(JSON.stringify(r.exec("abcd")) === '["a"]');
+assert(r.lastIndex === "lastIndex");
