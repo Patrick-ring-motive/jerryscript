89d8b40257e091fd3786b4b7f509c16cbb0f76e6
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:04:55 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/hash.js)

--- jerryscript/tests/jerry/hash.js
+++ jerryscript/tests/jerry/hash.js
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 a = {};
-a['12345']=1;
-a['13345']=3;
-a['sss45']=4;
+a['12345'] = 1;
+a['13345'] = 3;
+a['sss45'] = 4;
 a['1'] = 2;
 
-assert (a[12345] === 1);
-assert (a[1] === 2);
-assert (a[13345] === 3);
-assert (a['sss45'] === 4);
+assert(a[12345] === 1);
+assert(a[1] === 2);
+assert(a[13345] === 3);
+assert(a['sss45'] === 4);
