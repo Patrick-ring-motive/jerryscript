725b71b418ab2fd0d2fde1d80d9b75a426028061
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:54:44 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-species.js)

--- jerryscript/tests/jerry/array-species.js
+++ jerryscript/tests/jerry/array-species.js
@@ -23,14 +23,14 @@
 
 // Subclasses of Array construct themselves under map, etc
 
-class MyArray extends Array { }
+class MyArray extends Array {}
 
 function assertEquals(a, b) {
   assert(a === b);
 }
 
-assertEquals(MyArray, new MyArray().map(()=>{}).constructor);
-assertEquals(MyArray, new MyArray().filter(()=>{}).constructor);
+assertEquals(MyArray, new MyArray().map(() => {}).constructor);
+assertEquals(MyArray, new MyArray().filter(() => {}).constructor);
 assertEquals(MyArray, new MyArray().slice().constructor);
 assertEquals(MyArray, new MyArray().splice().constructor);
 assertEquals(MyArray, new MyArray().concat([1]).constructor);
@@ -39,11 +39,13 @@ assertEquals(1, new MyArray().concat([1])[0]);
 // Subclasses can override @@species to return the another class
 
 class MyOtherArray extends Array {
-  static get [Symbol.species]() { return MyArray; }
+  static get[Symbol.species]() {
+    return MyArray;
+  }
 }
 
-assertEquals(MyArray, new MyOtherArray().map(()=>{}).constructor);
-assertEquals(MyArray, new MyOtherArray().filter(()=>{}).constructor);
+assertEquals(MyArray, new MyOtherArray().map(() => {}).constructor);
+assertEquals(MyArray, new MyOtherArray().filter(() => {}).constructor);
 assertEquals(MyArray, new MyOtherArray().slice().constructor);
 assertEquals(MyArray, new MyOtherArray().splice().constructor);
 assertEquals(MyArray, new MyOtherArray().concat().constructor);
@@ -51,54 +53,60 @@ assertEquals(MyArray, new MyOtherArray().concat().constructor);
 // Array  methods on non-arrays return arrays
 
 class MyNonArray extends Array {
-  static get [Symbol.species]() { return MyObject; }
+  static get[Symbol.species]() {
+    return MyObject;
+  }
 }
 
-class MyObject { }
+class MyObject {}
 
 assertEquals(MyObject,
-             Array.prototype.map.call(new MyNonArray(), ()=>{}).constructor);
+  Array.prototype.map.call(new MyNonArray(), () => {}).constructor);
 assertEquals(MyObject,
-             Array.prototype.filter.call(new MyNonArray(), ()=>{}).constructor);
+  Array.prototype.filter.call(new MyNonArray(), () => {}).constructor);
 assertEquals(MyObject,
-             Array.prototype.slice.call(new MyNonArray()).constructor);
+  Array.prototype.slice.call(new MyNonArray()).constructor);
 assertEquals(MyObject,
-             Array.prototype.splice.call(new MyNonArray()).constructor);
+  Array.prototype.splice.call(new MyNonArray()).constructor);
 assertEquals(MyObject,
-             Array.prototype.concat.call(new MyNonArray()).constructor);
+  Array.prototype.concat.call(new MyNonArray()).constructor);
 
 assertEquals(undefined,
-             Array.prototype.map.call(new MyNonArray(), ()=>{}).length);
+  Array.prototype.map.call(new MyNonArray(), () => {}).length);
 assertEquals(undefined,
-             Array.prototype.filter.call(new MyNonArray(), ()=>{}).length);
+  Array.prototype.filter.call(new MyNonArray(), () => {}).length);
 // slice, splice, and concat actually do explicitly define the length.
 assertEquals(0, Array.prototype.slice.call(new MyNonArray()).length);
 assertEquals(0, Array.prototype.splice.call(new MyNonArray()).length);
-assertEquals(1, Array.prototype.concat.call(new MyNonArray(), ()=>{}).length);
+assertEquals(1, Array.prototype.concat.call(new MyNonArray(), () => {}).length);
 
 // Defaults when constructor or @@species is missing or non-constructor
 
 class MyDefaultArray extends Array {
-  static get [Symbol.species]() { return undefined; }
+  static get[Symbol.species]() {
+    return undefined;
+  }
 }
-assertEquals(Array, new MyDefaultArray().map(()=>{}).constructor);
+assertEquals(Array, new MyDefaultArray().map(() => {}).constructor);
 
-class MyOtherDefaultArray extends Array { }
+class MyOtherDefaultArray extends Array {}
 assertEquals(MyOtherDefaultArray,
-             new MyOtherDefaultArray().map(()=>{}).constructor);
+  new MyOtherDefaultArray().map(() => {}).constructor);
 MyOtherDefaultArray.prototype.constructor = undefined;
-assertEquals(Array, new MyOtherDefaultArray().map(()=>{}).constructor);
+assertEquals(Array, new MyOtherDefaultArray().map(() => {}).constructor);
 assertEquals(Array, new MyOtherDefaultArray().concat().constructor);
 
 // Exceptions propagated when getting constructor @@species throws
 
-class SpeciesError extends Error { }
-class ConstructorError extends Error { }
+class SpeciesError extends Error {}
+class ConstructorError extends Error {}
 class MyThrowingArray extends Array {
-  static get [Symbol.species]() { throw new SpeciesError; }
+  static get[Symbol.species]() {
+    throw new SpeciesError;
+  }
 }
 
-function assertThrows (a, b) {
+function assertThrows(a, b) {
   try {
     a();
   } catch (e) {
@@ -106,11 +114,13 @@ function assertThrows (a, b) {
   }
 }
 
-assertThrows(() => new MyThrowingArray().map(()=>{}), SpeciesError);
+assertThrows(() => new MyThrowingArray().map(() => {}), SpeciesError);
 Object.defineProperty(MyThrowingArray.prototype, 'constructor', {
-    get() { throw new ConstructorError; }
+  get() {
+    throw new ConstructorError;
+  }
 });
-assertThrows(() => new MyThrowingArray().map(()=>{}), ConstructorError);
+assertThrows(() => new MyThrowingArray().map(() => {}), ConstructorError);
 
 // Previously unexpected errors from setting properties in arrays throw
 
@@ -120,8 +130,8 @@ class FrozenArray extends Array {
     Object.freeze(this);
   }
 }
-assertThrows(() => new FrozenArray([1]).map(()=>0), TypeError);
-assertThrows(() => new FrozenArray([1]).filter(()=>true), TypeError);
+assertThrows(() => new FrozenArray([1]).map(() => 0), TypeError);
+assertThrows(() => new FrozenArray([1]).filter(() => true), TypeError);
 assertThrows(() => new FrozenArray([1]).slice(0, 1), TypeError);
 assertThrows(() => new FrozenArray([1]).splice(0, 1), TypeError);
 assertThrows(() => new FrozenArray([]).concat([1]), TypeError);
@@ -135,7 +145,7 @@ class MyObservedArray extends Array {
     super(...args);
     params = args;
   }
-  static get [Symbol.species]() {
+  static get[Symbol.species]() {
     count++
     return this;
   }
@@ -143,7 +153,7 @@ class MyObservedArray extends Array {
 
 function assertArrayEquals(value, expected, type) {
   assert(expected.length === value.length);
-  for (var i=0; i<value.length; ++i) {
+  for (var i = 0; i < value.length; ++i) {
     assertEquals(expected[i], value[i]);
   }
 }
@@ -151,34 +161,34 @@ function assertArrayEquals(value, expected, type) {
 count = 0;
 params = undefined;
 assertEquals(MyObservedArray,
-             new MyObservedArray().map(()=>{}).constructor);
+  new MyObservedArray().map(() => {}).constructor);
 assertEquals(1, count);
 assertArrayEquals([0], params);
 
 count = 0;
 params = undefined;
 assertEquals(MyObservedArray,
-             new MyObservedArray().filter(()=>{}).constructor);
+  new MyObservedArray().filter(() => {}).constructor);
 assertEquals(1, count);
 assertArrayEquals([0], params);
 
 count = 0;
 params = undefined;
 assertEquals(MyObservedArray,
-             new MyObservedArray().concat().constructor);
+  new MyObservedArray().concat().constructor);
 assertEquals(1, count);
 assertArrayEquals([0], params);
 
 count = 0;
 params = undefined;
 assertEquals(MyObservedArray,
-             new MyObservedArray().slice().constructor);
+  new MyObservedArray().slice().constructor);
 assertEquals(1, count);
 assertArrayEquals([0], params);
 
 count = 0;
 params = undefined;
 assertEquals(MyObservedArray,
-             new MyObservedArray().splice().constructor);
+  new MyObservedArray().splice().constructor);
 assertEquals(1, count);
 assertArrayEquals([0], params);
