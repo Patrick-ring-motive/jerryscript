5e11db0d34e73a647f42df6da0f72efd9fc7c590
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:01:17 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/regression-test-issue-3214.js)

--- jerryscript/tests/jerry/fail/regression-test-issue-3214.js
+++ jerryscript/tests/jerry/fail/regression-test-issue-3214.js
@@ -12,4 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var $ = { $ ( ){ } } = { g ( ) { ( ) => m ++ ( ) } }  = class extends C { constructor ( ) { super () } }
+var $ = {
+  $() {}
+} = {
+  g() {
+    () => m++()
+  }
+} = class extends C {
+  constructor() {
+    super()
+  }
+}
