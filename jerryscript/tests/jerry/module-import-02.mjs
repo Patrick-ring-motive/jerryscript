40d131202ea8628f792454b443b1b2b9e97ec877
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:07:58 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/module-import-02.mjs)

--- jerryscript/tests/jerry/module-import-02.mjs
+++ jerryscript/tests/jerry/module-import-02.mjs
@@ -14,11 +14,14 @@
  */
 
 import def, * as mod from "module-export-02.mjs";
-import {b_, c_,} from "module-export-02.mjs";
+import {
+  b_,
+  c_,
+} from "module-export-02.mjs";
 
-assert (def() === "default")
-assert (mod.aa === "a")
-assert (b_ === 5)
-assert (c_(b_) === 10)
-assert (mod.x === 42)
-assert (Array.isArray(mod.d))
+assert(def() === "default")
+assert(mod.aa === "a")
+assert(b_ === 5)
+assert(c_(b_) === 10)
+assert(mod.x === 42)
+assert(Array.isArray(mod.d))
