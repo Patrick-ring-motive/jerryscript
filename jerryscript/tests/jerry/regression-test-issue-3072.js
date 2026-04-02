eded9437959ecaa094ddb9049276ea57d69ba65a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:20:54 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-3072.js)

--- jerryscript/tests/jerry/regression-test-issue-3072.js
+++ jerryscript/tests/jerry/regression-test-issue-3072.js
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var arr = [ , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ];
-arr [4294967294] = 0
-assert (arr.length === 4294967295);
-assert (arr[4294967294] === 0);
+var arr = [, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ];
+arr[4294967294] = 0
+assert(arr.length === 4294967295);
+assert(arr[4294967294] === 0);
 
 var arrb = new ArrayBuffer(13);
 
 try {
   var d = new DataView(arrb, 12, -Infinity);
   d.setFloat32(1, 1);
-  assert (false);
+  assert(false);
 } catch (e) {
-  assert (e instanceof RangeError);
+  assert(e instanceof RangeError);
 }
