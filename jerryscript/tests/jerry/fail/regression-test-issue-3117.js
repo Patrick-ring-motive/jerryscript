f9775eddb609ef37ba6b248d35c8d1f925e9fe54
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:00:54 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/fail/regression-test-issue-3117.js)

--- jerryscript/tests/jerry/fail/regression-test-issue-3117.js
+++ jerryscript/tests/jerry/fail/regression-test-issue-3117.js
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var $ = class { } .prototype  = [ ]
-switch ( '1' ) {
+var $ = class {}.prototype = []
+switch ('1') {
