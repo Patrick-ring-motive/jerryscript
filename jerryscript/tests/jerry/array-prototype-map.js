6a3652050dc40d793b2ebd7a205fe26ef7ad9b73
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:54:08 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/array-prototype-map.js)

--- jerryscript/tests/jerry/array-prototype-map.js
+++ jerryscript/tests/jerry/array-prototype-map.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 // helper function - simple implementation
-Array.prototype.equals = function (array) {
+Array.prototype.equals = function(array) {
   if (this.length != array.length)
     return false;
 
@@ -21,9 +21,8 @@ Array.prototype.equals = function (array) {
     if (this[i] instanceof Array && array[i] instanceof Array) {
       if (!this[i].equals(array[i]))
         return false;
-      }
-      else if (this[i] != array[i]) {
-        return false;
+    } else if (this[i] != array[i]) {
+      return false;
     }
   }
 
@@ -34,43 +33,49 @@ Array.prototype.equals = function (array) {
 try {
   [0].map(new Object());
   assert(false);
-} catch(e) {
+} catch (e) {
   assert(e instanceof TypeError);
 }
 
 // various checks
-assert ([1, 4, 9].map(Math.sqrt).equals([1, 2, 3]));
+assert([1, 4, 9].map(Math.sqrt).equals([1, 2, 3]));
 
-assert (isNaN([1, 4, "X"].map(Number)[2]));
+assert(isNaN([1, 4, "X"].map(Number)[2]));
 
 var func = function(val, idx) {
   return val + idx;
 };
 
-assert ([1, 4, 9].map(func).equals([1,5,11]));
+assert([1, 4, 9].map(func).equals([1, 5, 11]));
 
-assert ([1, "X", 10].map(func).equals([1, "X1", 12]));
+assert([1, "X", 10].map(func).equals([1, "X1", 12]));
 
-var arr = [1,2,3];
+var arr = [1, 2, 3];
 arr.length = 5;
 assert(arr.map(func).length === arr.length);
 
 var long_array = [0, 1];
-assert (long_array.map(func).equals([0,2]));
+assert(long_array.map(func).equals([0, 2]));
 
 long_array[100] = 1;
-assert (long_array.map(func).equals([0,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,101]));
+assert(long_array.map(func).equals([0, 2, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , 101]));
 
-var arr = [1,2];
+var arr = [1, 2];
 Array.prototype[0] = 3;
-var newArr = arr.map(function(value) { return value; });
+var newArr = arr.map(function(value) {
+  return value;
+});
 delete Array.prototype[0];
 assert(newArr.hasOwnProperty("0"));
 assert(newArr[0] === 1);
 
 // check behavior when unable to get length
 var obj = {};
-Object.defineProperty(obj, 'length', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(obj, 'length', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 obj.map = Array.prototype.map;
 
 try {
@@ -84,7 +89,11 @@ try {
 // check behavior when unable to get element
 var obj = {}
 obj.length = 1;
-Object.defineProperty(obj, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+Object.defineProperty(obj, '0', {
+  'get': function() {
+    throw new ReferenceError("foo");
+  }
+});
 obj.map = Array.prototype.map
 
 try {
@@ -96,15 +105,21 @@ try {
 }
 
 // check thisArg
-var thisArg = {add: 10};
+var thisArg = {
+  add: 10
+};
 var func2 = function(value) {
   return this.add + value;
 }
-assert([1,2].map(func2, thisArg).equals([11, 12]));
+assert([1, 2].map(func2, thisArg).equals([11, 12]));
 
 // check passed Object
-var array_example = [1,2];
-Object.defineProperty(array_example, 'const', { 'get' : function () {return "CT";} });
+var array_example = [1, 2];
+Object.defineProperty(array_example, 'const', {
+  'get': function() {
+    return "CT";
+  }
+});
 var func3 = function(value, idx, thisobj) {
   return value * idx + thisobj.const;
 }
