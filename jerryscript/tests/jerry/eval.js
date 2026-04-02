256231b3a1fc35a9154e877ffd308664b7fdd0f5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:57:59 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/eval.js)

--- jerryscript/tests/jerry/eval.js
+++ jerryscript/tests/jerry/eval.js
@@ -12,116 +12,104 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-assert (eval () === undefined);
-assert (eval (undefined) === undefined);
-assert (eval (null) === null);
-assert (eval (true) === true);
-assert (eval (false) === false);
-assert (eval (1) === 1);
-assert (eval (eval) === eval);
+assert(eval() === undefined);
+assert(eval(undefined) === undefined);
+assert(eval(null) === null);
+assert(eval(true) === true);
+assert(eval(false) === false);
+assert(eval(1) === 1);
+assert(eval(eval) === eval);
 
 /* Indirect eval */
-function f1()
-{
- var v1 = 'local value';
+function f1() {
+  var v1 = 'local value';
 
- assert (v1 === 'local value');
- assert (typeof (this.v1) === 'undefined');
+  assert(v1 === 'local value');
+  assert(typeof(this.v1) === 'undefined');
 
- r = this.eval ('var v1 = "global value";');
+  r = this.eval('var v1 = "global value";');
 
- assert (v1 === 'local value');
- assert (this.v1 === 'global value');
- assert (r === undefined);
+  assert(v1 === 'local value');
+  assert(this.v1 === 'global value');
+  assert(r === undefined);
 };
 
-f1 ();
+f1();
 
 /* Direct eval from strict mode code */
-function f2 (global)
-{
- 'use strict';
- var v2 = 'local value';
+function f2(global) {
+  'use strict';
+  var v2 = 'local value';
 
- assert (v2 === 'local value');
- assert (typeof (global.v2) === 'undefined');
+  assert(v2 === 'local value');
+  assert(typeof(global.v2) === 'undefined');
 
- r = eval ('var v2 = "global value";');
+  r = eval('var v2 = "global value";');
 
- assert (v2 === 'local value');
- assert (typeof (global.v2) === 'undefined');
- assert (r === undefined);
+  assert(v2 === 'local value');
+  assert(typeof(global.v2) === 'undefined');
+  assert(r === undefined);
 
- try
- {
-   eval ('arguments = 1;');
-   assert (false);
- }
- catch (e)
- {
-   assert (e instanceof SyntaxError);
- }
+  try {
+    eval('arguments = 1;');
+    assert(false);
+  } catch (e) {
+    assert(e instanceof SyntaxError);
+  }
 }
 
-f2 (this);
+f2(this);
 
 var y;
 
-for (var i = 0; i < 100; i++)
-{
-  var r = eval ('var x =' + ' 1;');
-  assert (typeof (x) === 'number');
-  assert (r === undefined);
+for (var i = 0; i < 100; i++) {
+  var r = eval('var x =' + ' 1;');
+  assert(typeof(x) === 'number');
+  assert(r === undefined);
 
   delete x;
-  assert (typeof (x) === 'undefined');
+  assert(typeof(x) === 'undefined');
 
-  r = eval ('"use ' + 's' + 't' + 'r' + 'i' + 'c' + 't"; va' + 'r x = 1;');
-  assert (typeof (x) === 'undefined');
-  assert (r === "use strict");
+  r = eval('"use ' + 's' + 't' + 'r' + 'i' + 'c' + 't"; va' + 'r x = 1;');
+  assert(typeof(x) === 'undefined');
+  assert(r === "use strict");
 
   y = 'str';
-  assert (typeof (y) === 'string');
+  assert(typeof(y) === 'string');
 
   delete y;
-  assert (typeof (y) === 'string');
+  assert(typeof(y) === 'string');
 
-  r = eval ('var y = "another ' + 'string";');
-  assert (y === 'another string');
-  assert (r == undefined);
+  r = eval('var y = "another ' + 'string";');
+  assert(y === 'another string');
+  assert(r == undefined);
 
   delete y;
-  assert (typeof (y) === 'string');
+  assert(typeof(y) === 'string');
 
-  r = eval ('if (true) 3; else 5;');
-  assert (r === 3);
+  r = eval('if (true) 3; else 5;');
+  assert(r === 3);
 }
 
 // Check SyntaxError handling
-try
-{
-  eval ('var var;');
-  assert (false);
-}
-catch (e)
-{
-  assert (e instanceof SyntaxError);
+try {
+  eval('var var;');
+  assert(false);
+} catch (e) {
+  assert(e instanceof SyntaxError);
 }
 
-try
-{
-  eval ("v_0 = {a: Math, /[]/};");
-  assert (false);
-}
-catch(e)
-{
-  assert (e instanceof SyntaxError);
+try {
+  eval("v_0 = {a: Math, /[]/};");
+  assert(false);
+} catch (e) {
+  assert(e instanceof SyntaxError);
 }
 
 // nested eval with function expressions
 code = 'eval("(function (){})")';
 code = "eval ('" + code + "')";
-eval (code);
+eval(code);
 
 // Eval enclosed in brackets is still an eval.
 var p1 = 0;
@@ -154,8 +142,11 @@ function f6() {
 f6();
 
 y = 1;
+
 function f7() {
-  function x() { return y; }
+  function x() {
+    return y;
+  }
   eval("assert(x()() === 5); function y() { return 5 } assert(x()() === 5)");
 }
 f7()
