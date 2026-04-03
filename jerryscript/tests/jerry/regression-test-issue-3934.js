46952e2e1a9b7700e7f58ff0f331f61a477c7199
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:16:21 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-3934.js)

--- jerryscript/tests/jerry/regression-test-issue-3934.js
+++ jerryscript/tests/jerry/regression-test-issue-3934.js
@@ -14,6 +14,6 @@
 
 var r = /(?:(?:^b?)*)*a/.exec("bbba")
 
-assert (r !== null);
-assert (r.index === 3);
-assert (r[0] === "a");
+assert(r !== null);
+assert(r.index === 3);
+assert(r[0] === "a");
