13656a1b2effa9e9a72d0178cce9f23229c4cd92
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:22:59 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/try-catch-finally.js)

--- jerryscript/tests/jerry/try-catch-finally.js
+++ jerryscript/tests/jerry/try-catch-finally.js
@@ -12,28 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var was_catch = false, was_finally = false;
+var was_catch = false,
+  was_finally = false;
 
-try {
-} catch (err) {
+try {} catch (err) {
   was_catch = true;
 } finally {
   was_finally = true;
 }
 
-assert (!was_catch && was_finally);
+assert(!was_catch && was_finally);
 
 was_catch = false;
 was_finally = false;
 
 try {
   throw 1;
-  assert (0);
+  assert(0);
 } catch (err) {
-  assert (err === 1);
+  assert(err === 1);
   was_catch = true;
 } finally {
   was_finally = true;
 }
 
-assert (was_catch && was_finally);
+assert(was_catch && was_finally);
