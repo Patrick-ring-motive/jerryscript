c981963e1c13212684f25238ebeff03dd1c7128c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:21:01 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/string-prototype-padding.js)

--- jerryscript/tests/jerry/string-prototype-padding.js
+++ jerryscript/tests/jerry/string-prototype-padding.js
@@ -17,10 +17,10 @@ var test = "bar"
 
 assert(test.padStart("5", "foo") === "fobar")
 assert(test.padStart(6, "foo") === "foobar")
-assert(test.padStart(8, '1234')=== "12341bar")
+assert(test.padStart(8, '1234') === "12341bar")
 assert(test.padEnd(5, "baz") === "barba")
 assert(test.padEnd(6, "baz") === "barbaz")
-assert(test.padEnd(8, '1234')=== "bar12341")
+assert(test.padEnd(8, '1234') === "bar12341")
 
 // Check for negative value
 assert(test.padStart(-5, "foo") === "bar")
@@ -36,20 +36,20 @@ assert(test.padStart(10, "") === "bar")
 
 // Check with unicode surrogate characters
 // 𐋀 = [55296, 57024]
-var unicode_padded = "a".padStart(4 ,"𐋀")
+var unicode_padded = "a".padStart(4, "𐋀")
 assert(unicode_padded.charCodeAt(0) == 55296)
 assert(unicode_padded.charCodeAt(1) == 57024)
 assert(unicode_padded.charCodeAt(2) == 55296)
 assert(unicode_padded.charCodeAt(3) == 97)
 
-unicode_padded = "a".padEnd(4 ,"𐋀")
+unicode_padded = "a".padEnd(4, "𐋀")
 assert(unicode_padded.charCodeAt(0) == 97)
 assert(unicode_padded.charCodeAt(1) == 55296)
 assert(unicode_padded.charCodeAt(2) == 57024)
 assert(unicode_padded.charCodeAt(3) == 55296)
 
 // 𐋂 = [55296, 57026]
-unicode_padded = "𐋂".padStart(4 ,"𐋀")
+unicode_padded = "𐋂".padStart(4, "𐋀")
 assert(unicode_padded.charCodeAt(0) == 55296)
 assert(unicode_padded.charCodeAt(1) == 57024)
 assert(unicode_padded.charCodeAt(2) == 55296)
@@ -57,26 +57,26 @@ assert(unicode_padded.charCodeAt(3) == 57026)
 
 // Check for errors in length
 try {
-  test.padStart(Symbol("Will this fail?"), "It should" )
+  test.padStart(Symbol("Will this fail?"), "It should")
   assert(false);
 } catch (e) {
   assert(e instanceof TypeError);
 }
 try {
-  test.padEnd(Symbol("Will this fail?"), "It should" )
+  test.padEnd(Symbol("Will this fail?"), "It should")
   assert(false);
 } catch (e) {
   assert(e instanceof TypeError);
 }
 // Check for errors in fillString
 try {
-  test.padStart(10, Symbol("Fail, this should. " ))
+  test.padStart(10, Symbol("Fail, this should. "))
   assert(false);
 } catch (e) {
   assert(e instanceof TypeError);
 }
 try {
-  test.padEnd(10, Symbol("Fail, this should. " ))
+  test.padEnd(10, Symbol("Fail, this should. "))
   assert(false);
 } catch (e) {
   assert(e instanceof TypeError);
