b45920cff525604736baa58c384378f0210e08c2
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:16:52 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4051.js)

--- jerryscript/tests/jerry/regression-test-issue-4051.js
+++ jerryscript/tests/jerry/regression-test-issue-4051.js
@@ -14,8 +14,9 @@
 
 function foo() {
   for (var i = 0; i < 3; i++) {
-    const N = class { };
-    function bar(a0) { }
+    const N = class {};
+
+    function bar(a0) {}
     assert(N.name === 'N');
   }
 }
