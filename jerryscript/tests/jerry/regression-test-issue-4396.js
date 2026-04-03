97fed5bfb71d8b34e6d8f6ab2b182407f7b14c01
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:17:37 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4396.js)

--- jerryscript/tests/jerry/regression-test-issue-4396.js
+++ jerryscript/tests/jerry/regression-test-issue-4396.js
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function demo() {
-}
+function demo() {}
 
 var proxy = new Proxy(demo, {});
 var weakset = new WeakSet();
