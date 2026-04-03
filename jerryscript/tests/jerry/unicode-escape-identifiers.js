a6a6663435fa60ab08bdbb988e400859b5d7b125
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:24:02 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/unicode-escape-identifiers.js)

--- jerryscript/tests/jerry/unicode-escape-identifiers.js
+++ jerryscript/tests/jerry/unicode-escape-identifiers.js
@@ -15,18 +15,22 @@
 var \u{102C0} = 2;
 assert(\u{102C0} === 2);
 
-var o1 = { \u{102C0} : 3 };
+var o1 = {
+  \u{102C0}: 3
+};
 assert(o1['\ud800\udec0'] === 3);
 
-var o2 = { '\ud800\udec0' : 4 };
+var o2 = {
+  '\ud800\udec0': 4
+};
 assert(o2.\u{102C0} === 4);
 
 try {
   eval('var ⸯ');
   assert(false);
-} catch(e) {
+} catch (e) {
   assert(e instanceof SyntaxError);
 }
 
-var 𐋀 = 5;
+var𐋀 = 5;
 assert(𐋀 === 5);
