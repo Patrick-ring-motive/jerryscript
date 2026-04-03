7388d37530552a94275ac6f5407b48976c35f4c1
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:22:54 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/to-length.js)

--- jerryscript/tests/jerry/to-length.js
+++ jerryscript/tests/jerry/to-length.js
@@ -28,14 +28,20 @@ function compareArray(a, b) {
 // concat
 (function() {
   try {
-    Array.prototype.concat.call({}, {[Symbol.isConcatSpreadable]: true, length : 2 **53 - 1})
+    Array.prototype.concat.call({}, {
+      [Symbol.isConcatSpreadable]: true,
+      length: 2 ** 53 - 1
+    })
     assert(false);
   } catch (e) {
     assert(e instanceof TypeError);
   }
 
   try {
-    Array.prototype.concat.call([1, 2, 3, 4], {[Symbol.isConcatSpreadable]: true, length : 2 **53 - 4})
+    Array.prototype.concat.call([1, 2, 3, 4], {
+      [Symbol.isConcatSpreadable]: true,
+      length: 2 ** 53 - 4
+    })
     assert(false);
   } catch (e) {
     assert(e instanceof TypeError);
@@ -44,7 +50,10 @@ function compareArray(a, b) {
 
 // fill
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo'
+  }
   Array.prototype.fill.call(obj, 'bar', 2 ** 53 - 5)
 
   assert(obj[2 ** 53 - 5] === 'bar');
@@ -52,7 +61,10 @@ function compareArray(a, b) {
   assert(obj[2 ** 53 - 3] === 'bar');
   assert(obj[2 ** 53 - 2] === 'bar');
 
-  obj = { length: 2 ** 53 + 2, [2 ** 53 - 2] : 'foo'}
+  obj = {
+    length: 2 ** 53 + 2,
+    [2 ** 53 - 2]: 'foo'
+  }
   Array.prototype.fill.call(obj, 'bar', 2 ** 53 - 2)
 
   assert(obj[2 ** 53 - 2] === 'bar');
@@ -61,35 +73,58 @@ function compareArray(a, b) {
 
 // includes
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo'
+  }
   assert(Array.prototype.includes.call(obj, 'foo', 2 ** 53 - 10) === true);
 
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 + 5] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 + 5]: 'foo'
+  }
   assert(Array.prototype.includes.call(obj, 'foo', 2 ** 53 - 10) === false);
 })();
 
 // indexOf
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo'
+  }
   assert(Array.prototype.indexOf.call(obj, 'foo', 2 ** 53 - 10) === 2 ** 53 - 5);
 
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 + 5] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 + 5]: 'foo'
+  }
   assert(Array.prototype.indexOf.call(obj, 'foo', 2 ** 53 - 10) === -1);
 })();
 
 // lastIndexOf
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo', [2 ** 53 - 6] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo',
+    [2 ** 53 - 6]: 'foo'
+  }
   assert(Array.prototype.lastIndexOf.call(obj, 'foo', 2 ** 53 - 2) === 2 ** 53 - 5);
 
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo', [2 ** 53 - 6] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo',
+    [2 ** 53 - 6]: 'foo'
+  }
   assert(Array.prototype.lastIndexOf.call(obj, 'foo', 10) === -1);
 })();
 
-
 // pop
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 2] : 'foo', [2 ** 53 - 3] : 'bar'}
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 2]: 'foo',
+    [2 ** 53 - 3]: 'bar'
+  }
   assert(Array.prototype.pop.call(obj) === 'foo');
   assert(obj.length === 2 ** 53 - 2);
 
@@ -99,7 +134,9 @@ function compareArray(a, b) {
 
 // push
 (function() {
-  var obj = { length: 2 ** 53 - 1 };
+  var obj = {
+    length: 2 ** 53 - 1
+  };
   try {
     Array.prototype.push.call(obj, 'foo');
     assert(false);
@@ -107,7 +144,10 @@ function compareArray(a, b) {
     assert(e instanceof TypeError);
   }
 
-  var obj = { length: 2 ** 53 - 2, [2 ** 53 - 3] : 'foo'}
+  var obj = {
+    length: 2 ** 53 - 2,
+    [2 ** 53 - 3]: 'foo'
+  }
 
   assert(Array.prototype.push.call(obj, 'bar') === 2 ** 53 - 1);
   assert(obj.length === 2 ** 53 - 1);
@@ -115,19 +155,27 @@ function compareArray(a, b) {
 
 // slice
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo', [2 ** 53 - 2] : 'bar'};
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo',
+    [2 ** 53 - 2]: 'bar'
+  };
   var sliced = Array.prototype.slice.call(obj, 2 ** 53 - 10);
-  assert(compareArray(sliced, [,,,,,'foo',,,'bar']));
+  assert(compareArray(sliced, [, , , , , 'foo', , , 'bar']));
 
   var sliced = Array.prototype.slice.call(obj, 2 ** 53 - 8, 2 ** 53 - 4);
-  assert(compareArray(sliced, [,,,'foo',]));
+  assert(compareArray(sliced, [, , , 'foo', ]));
 })();
 
 // splice
 (function() {
-  var obj = { length: 2 ** 53 - 1, [2 ** 53 - 5] : 'foo', [2 ** 53 - 2] : 'bar'};
+  var obj = {
+    length: 2 ** 53 - 1,
+    [2 ** 53 - 5]: 'foo',
+    [2 ** 53 - 2]: 'bar'
+  };
   var spliced = Array.prototype.splice.call(obj, 2 ** 53 - 10, 5, '1', '2', '3', '4');
-  assert(compareArray(spliced, [,,,,,]));
+  assert(compareArray(spliced, [, , , , , ]));
   assert(obj.length === 2 ** 53 - 2);
   assert(obj[2 ** 53 - 3] === 'bar');
   assert(obj[2 ** 53 - 6] === 'foo');
@@ -139,7 +187,9 @@ function compareArray(a, b) {
 
 // unshift
 (function() {
-  var obj = { length: 2 ** 53 - 1 };
+  var obj = {
+    length: 2 ** 53 - 1
+  };
   try {
     Array.prototype.unshift.call(obj, 'foo');
     assert(false);
