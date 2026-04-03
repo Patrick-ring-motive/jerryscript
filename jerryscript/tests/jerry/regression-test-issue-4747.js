346382572e3b1e829e16ff7379b0861679b1dc34
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:18:25 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4747.js)

--- jerryscript/tests/jerry/regression-test-issue-4747.js
+++ jerryscript/tests/jerry/regression-test-issue-4747.js
@@ -14,19 +14,19 @@
 
 var a = new Int32Array(256);
 try {
-    a.sort(function () {
-        var o = new Proxy(this, {
-            has: function () {},
-            get: function () {
-                a = true;
-                return 30;
-            }
-        });
-        var result = "";
-        for (var p in o)
-            result += o[p];
+  a.sort(function() {
+    var o = new Proxy(this, {
+      has: function() {},
+      get: function() {
+        a = true;
+        return 30;
+      }
     });
-    assert(false);
+    var result = "";
+    for (var p in o)
+      result += o[p];
+  });
+  assert(false);
 } catch (e) {
-    assert(e instanceof TypeError);
+  assert(e instanceof TypeError);
 }
