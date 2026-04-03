f10b3de2b94d942e6a159250dfa602ac4772d980
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:19:14 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-652.js)

--- jerryscript/tests/jerry/regression-test-issue-652.js
+++ jerryscript/tests/jerry/regression-test-issue-652.js
@@ -15,17 +15,17 @@
 try {
   eval("this / 10");
 } catch (e) {
-  assert (false);
+  assert(false);
 }
 
 try {
   eval("var v_0 = 10;\nv_0++ / 1");
 } catch (e) {
-  assert (false);
+  assert(false);
 }
 
 try {
   eval("var v_0 = 10;\nif (v_0++ / 1) {\n}");
 } catch (e) {
-  assert (false);
+  assert(false);
 }
