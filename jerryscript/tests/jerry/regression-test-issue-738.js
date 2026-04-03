983aa4bf7e6a0cf50fa2a0b55046352c2b17190a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:19:41 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-738.js)

--- jerryscript/tests/jerry/regression-test-issue-738.js
+++ jerryscript/tests/jerry/regression-test-issue-738.js
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function g() {};for (B in Boolean) print(f);
+function g() {};
+for (B in Boolean) print(f);
