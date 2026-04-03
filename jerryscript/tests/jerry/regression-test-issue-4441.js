0afab5e4bb9b43c6e01b409200c7a0142368f054
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:17:59 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4441.js)

--- jerryscript/tests/jerry/regression-test-issue-4441.js
+++ jerryscript/tests/jerry/regression-test-issue-4441.js
@@ -15,7 +15,18 @@
 var get = [];
 var constructor = Function();
 constructor[Symbol.species] = Object;
-var p = new Proxy({ constructor: constructor, flags: '', exec: function() { return /\ua796/iu; } }, { defineProperty: function(o, k) { get.push(k); return o[k]; }});
+var p = new Proxy({
+  constructor: constructor,
+  flags: '',
+  exec: function() {
+    return /\ua796/iu;
+  }
+}, {
+  defineProperty: function(o, k) {
+    get.push(k);
+    return o[k];
+  }
+});
 
 try {
   RegExp.prototype[Symbol.split].call(p, 7996);
