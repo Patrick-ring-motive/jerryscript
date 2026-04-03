1881f673abedfe620a78f8ffbd65b5cc04f79f70
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:15:40 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-3837.js)

--- jerryscript/tests/jerry/regression-test-issue-3837.js
+++ jerryscript/tests/jerry/regression-test-issue-3837.js
@@ -12,22 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function dummy ()
-{
+function dummy() {
   "D" + "a";
 }
 
-var p1Handler = { getOwnPropertyDescriptor: dummy };
+var p1Handler = {
+  getOwnPropertyDescriptor: dummy
+};
 var p1 = new Proxy({}, p1Handler);
 
-function dummyString ()
-{
+function dummyString() {
   return "a";
 }
 
-var p2Handler = { deleteProperty: dummyString };
+var p2Handler = {
+  deleteProperty: dummyString
+};
 var p2 = new Proxy(p1, p2Handler);
 
 var result = (delete p2[1]);
 
-assert (result);
+assert(result);
