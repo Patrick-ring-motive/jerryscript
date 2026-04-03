1cd2d26c4003d52e99bb4043fb07231067b8adfc
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:15:18 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-3817.js)

--- jerryscript/tests/jerry/regression-test-issue-3817.js
+++ jerryscript/tests/jerry/regression-test-issue-3817.js
@@ -13,4 +13,8 @@
 // limitations under the License.
 
 var replace = RegExp.prototype[Symbol.replace];
-replace.call({ exec : ( ) => { return {  } } }, '^o𓙦һ', "a");
+replace.call({
+  exec: () => {
+    return {}
+  }
+}, '^o𓙦һ', "a");
