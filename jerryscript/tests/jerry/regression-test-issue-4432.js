2bea067366c0b0a4b4f12d13d84dfa6c1e04b055
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:17:54 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4432.js)

--- jerryscript/tests/jerry/regression-test-issue-4432.js
+++ jerryscript/tests/jerry/regression-test-issue-4432.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function f(x,x,x) {
+function f(x, x, x) {
   return arguments[1];
 }
 
