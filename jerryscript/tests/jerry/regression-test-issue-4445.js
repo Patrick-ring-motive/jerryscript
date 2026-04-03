ebf0df8965d52957d706b2378c6deb6104ec131c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:18:01 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4445.js)

--- jerryscript/tests/jerry/regression-test-issue-4445.js
+++ jerryscript/tests/jerry/regression-test-issue-4445.js
@@ -14,37 +14,35 @@
 
 // Keeping test as it is due to the fact this was triggered via a gc related issue
 
-var a= ["", "\0", "\t", "\n", "\v", "\f", "\r", " ", "\u00a0", "\u2028", "\u2029", "\ufeff"]
+var a = ["", "\0", "\t", "\n", "\v", "\f", "\r", " ", "\u00a0", "\u2028", "\u2029", "\ufeff"]
 Array.prototype[4] = 10;
 
-function Test()
-{
+function Test() {
   a.sort(function() {
-    var A = function() { };
+    var A = function() {};
     A.prototype.x = 42;
     var o = new Proxy({
-        "3": {
-          writable:false,
-          value:20
-        }
-      }, /*handler*/ {
-        getPrototypeOf: function (val, size, ch) {
-          var result = new String(val);
-          if (ch == null) {
-            ch = " ";
-          }
-          while (result.length < size) {
-            result = ch + result;
-          }
-          return result;
-        }
+      "3": {
+        writable: false,
+        value: 20
       }
-    );
+    }, /*handler*/ {
+      getPrototypeOf: function(val, size, ch) {
+        var result = new String(val);
+        if (ch == null) {
+          ch = " ";
+        }
+        while (result.length < size) {
+          result = ch + result;
+        }
+        return result;
+      }
+    });
 
     o.x = 43;
     var result = "";
     for (var p in o) {
-        result += o[p];
+      result += o[p];
     }
     return a | 0;
   });
@@ -56,5 +54,5 @@ try {
   Test();
   assert(false);
 } catch (ex) {
-  assert (ex instanceof EvalError);
+  assert(ex instanceof EvalError);
 }
