482c3bd29deef609a687170238c5ff07797030ad
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:18:13 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4468.js)

--- jerryscript/tests/jerry/regression-test-issue-4468.js
+++ jerryscript/tests/jerry/regression-test-issue-4468.js
@@ -15,5 +15,5 @@
 var str = 'for (let i=0; i<(eval("1; function x() { }; 2;")); x - i++) { x += delete x;}'
 
 var e = eval(str)
-assert(e === 'function x() { }true'
-       || e === 'function () { /* ecmascript */ }true');
+assert(e === 'function x() { }true' ||
+  e === 'function () { /* ecmascript */ }true');
