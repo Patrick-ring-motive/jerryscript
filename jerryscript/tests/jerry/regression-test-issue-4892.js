62fa3c3c4dc06500e0773c5597f93a5cbceb3f46
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 23:18:40 -0500

    style: js-beautify formatting (jerryscript/tests/jerry/regression-test-issue-4892.js)

--- jerryscript/tests/jerry/regression-test-issue-4892.js
+++ jerryscript/tests/jerry/regression-test-issue-4892.js
@@ -12,5 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-class MyArray extends class C{}
-{};
+class MyArray extends class C {} {};
